ef94997780f5f7134300aab9e361155c7cab7d3e
CrunchBang LiveUser <crunchbang@crunchbang.(none)>
2015-04-16 08:27:45 +0000

    Tested debug_listener.py, fixed some mistakes

--- input/debug_listener.py
+++ input/debug_listener.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
 from evdev import InputDevice, list_devices, categorize, ecodes
 import threading
 
 def listen_for_events(dev):
     for event in dev.read_loop():
         if event.type == ecodes.EV_KEY:
-            print dev.name+":  "+categorize(event)
+            print dev.name+":  "+str(categorize(event))
 
 devices = [InputDevice(fn) for fn in list_devices()]
+print devices
 for dev in devices:
     print dev.name+" - "+dev.fn
     thread = threading.Thread(target=listen_for_events, args=(dev,))
-    thread.daemon = True
+    thread.daemon = False
     thread.start()
+
+#while True:
+#    pass
