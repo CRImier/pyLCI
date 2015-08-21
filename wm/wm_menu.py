f8a05cd21f9bce6cac633707d99a97007cb4aa53
CRImier <crimier@yandex.ru>
2015-08-21 06:57:34 +0300

    Modified WM menu wrapper so that app switching is done correctly and the active app list gets updated every time WM menu appears on the screen

--- wm/wm_menu.py
+++ wm/wm_menu.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from wm import window_manager
 from menu import menu
 
@@ -13,43 +15,47 @@ class WindowManagerMenu(Menu):
         self.listener._set_wm_callback('KEY_KPSLASH', 'deactivate_active_app', self.wm)
 
     def activate(self):
-        self.refresh_elements()
         Menu.activate(self)
+
+    def to_foreground(self):
+        Menu.to_foreground(self)
+        self.refresh_elements()
+
+    def refresh(self):
+        self.refresh_elements()
+        Menu.refresh(self)
 
     def refresh_elements(self):
         self.contents = self.make_menu_items()
 
     def select_element(self):
-        self.refresh_elements()
         Menu.select_element(self)    
         
     def deactivate(self):
         Menu.deactivate(self)
 
     def move_up(self):
-        self.refresh_elements()
         Menu.move_up(self)
-        self.refresh()
 
     def move_down(self):
-        self.refresh_elements()
         Menu.move_down(self)
-        self.refresh()
 
     def make_menu_items(self):
         app_list = self.wm.get_app_list()
         menu_items = []
         for index, app in enumerate(app_list): #those app numbers might be a possibility for a disaster, TODO
-            app_item = [app.name, lambda: self.select_app(index)]
+            app_item = [app.name, self.select_app_wrapper]
             menu_items.append(app_item)
         return menu_items
 
+    def select_app_wrapper(self):
+        self.select_app(self.pointer)
+
     def select_app(self, number):
+        print(self.pointer)
         print("App number {0} selected".format(number))
         self.to_background
         self.wm.activate_app(number)
         while self.wm.active_app != None:
             sleep(1)
-        print "Finished, going back to menu"
-        self.refresh_elements()
-        self.refresh()
+        print("Finished, going back to menu")
