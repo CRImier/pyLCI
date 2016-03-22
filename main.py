446a3e48251750146a6567dfecc3aaa849357e2c
CRImier <crimier@yandex.ru>
2016-03-22 04:37:37 +0000

    Massive re-work

--- main.py
+++ main.py
@@ -1,63 +1,54 @@
 #!/usr/bin/env python
-
-from input import input
-from time import sleep
-from menu.menu import Menu
+import sys
+#Welcome to pyLCS innards
+#Here, things are about i and o, which are input and output
+#And we output things for debugging, so o goes first.
 from output import output
+
+#These lines are here so that welcome screen stays on a little longer:
+o = output.screen
+o.display_data("Welcome to", "pyLCS")
+
+try:
+    #All the LCS-related modules are imported here
+    from input import input
+    from menu.menu import Menu
+    #Now we init the input.
+    input.init(o)
+    i = input.listener
+    i.listen_direct()
+    #from apps import app_list
+except:
+    o.display_data("Oops. :(", "y u make mistake") #Yeah, that's about all the debug data. 
+    #import time;time.sleep(3) #u make mi sad i go to slip
+    #o.clear()
+    raise
+
+#Here go all native Python modules. It's close to impossible to screw that up.
 from subprocess import call
-
-
-listener = input.listener
-screen = output.screen
-listener.listen_direct()
-
-#For testing only
-#from wlan import wicd_int
-#wicd_int.Menu = Menu
-
-def send_sms(name):
-    screen.display_data("Sending SMS to:", name)
-    sleep(2)
-    screen.display_data("Success!", "")
-    sleep(2)
-
-def switch_music():
-    pass
-
-music_menu_contents = [
-["Next track", switch_music],
-["Previous track", switch_music],
-["Random track", switch_music],
-["Exit", "exit"]
+from time import sleep
+#import logging
+app_menu_contents = [
+["App1", lambda: True]
 ]
+try:
+    from apps.shutdown import main as shutdown
+    shutdown.init_app(i, o)
+    app_menu_contents.append([shutdown.menu_name, shutdown.callback])
+except:
+    o.display_data("Oi", "it crashed")
+    raise
 
-sms_menu_contents = [
-["SMS to Alice", lambda: send_sms("Alice")],
-["SMS to Bob", lambda: send_sms("Bob")],
-["SMS to Eve", lambda: send_sms("Eve")],
-["Exit", "exit"]
-]
 
-sms_menu = Menu(sms_menu_contents, screen, listener, "SMS menu")
-music_menu = Menu(music_menu_contents, screen, listener, "Music menu")
-
-main_menu_contents = [
-["Send SMS", sms_menu.activate],
-#["Connect to WiFi", lambda: wicd_int.WicdUserInterface(screen, listener).choose_networks()],
-["Music control", music_menu.activate],
-["Shutdown", lambda:call(['shutdown', '-h', 'now'])],
-]
-
-main_menu = Menu(main_menu_contents, screen, listener, "Main menu")
+app_menu = Menu(app_menu_contents, o, i, "App menu")
 
 def start():
     try:
-        main_menu.activate()
-    except:
-        try:
-            input.driver.deactivate()
-        except:
-            pass
+        app_menu.activate()
+    except KeyboardInterrupt:
+        input.driver.stop() #Might be needed for some input drivers, such as PiFaceCAD. 
+        o.display_data("Does Ctrl+C", "hurt scripts?")
+        sys.exit(1)
 
 if __name__ == "__main__":
     start()
