61589f26aa71359da34cd7656358e6d14a803adc
CRImier <crimier@yandex.ru>
2016-03-22 09:21:22 +0000

    Fixed update script so it would work and force overwrite things

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 INSTALL_DIR="/opt/lcs"
 SUDO=''
 if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 git pull
-$SUDO cp ./. $INSTALL_DIR -R
+$SUDO /bin/cp -Rrf ./. $INSTALL_DIR 
 $SUDO systemctl restart lcs.service
 
