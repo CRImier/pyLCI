07b95bfdb72fb36c0ec26592b1e7230cd234d666
CRImier <crimier@yandex.ru>
2016-03-22 04:45:22 +0000

    New setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-#apt-get install python python-dev python-serial python-pip
-#pip install evdev
-apt-get install gammu gammu-doc python-gammu python-gammu-doc
+INSTALL_DIR="/opt/lcs"
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+$SUDO apt-get install python python-dev python-serial python-pip
+$SUDO pip install evdev
+$SUDO mkdir -p $INSTALL_DIR
+$SUDO cp ./. $INSTALL_DIR -R
+cd $INSTALL_DIR
+$SUDO cp lcs.service /etc/systemd/system/
+$SUDO systemctl daemon-reload
+$SUDO systemctl enable lcs.service
+#Start it maybe?
 
