a0fb3fa32613fccfafc3f564f324cbd80534e817
CRImier <crimier@yandex.ru>
2016-03-25 20:29:29 +0000

    Moved part of aplication loading code into functions Added single-app mode Added command-line argument for single-app mode

--- main.py
+++ main.py
@@ -27,28 +27,54 @@ except:
 #Here go all native Python modules. It's close to impossible to screw that up.
 from subprocess import call
 from time import sleep
-#import logging
-app_menu_contents = [
-["App1", lambda: True]
-]
-try:
-    from apps.shutdown import main as shutdown
-    shutdown.init_app(i, o)
-    app_menu_contents.append([shutdown.menu_name, shutdown.callback])
-except:
-    o.display_data("Oi", "it crashed")
-    raise
+import importlib
+import argparse
 
+#Now we go and import all the apps.
+import apps
+app_list = {}
 
-app_menu = Menu(app_menu_contents, o, i, "App menu")
+def launch(name=None):
+    if name != None:
+        app = load_app(name)
+        ctrl_c_wrapper(app.callback)
+    else:
+        app_menu_contents = load_all_apps()
+        app_menu = Menu(app_menu_contents, o, i, "App menu")
+        ctrl_c_wrapper(app_menu.activate)
 
-def start():
+def ctrl_c_wrapper(callback):
     try:
-        app_menu.activate()
+        callback()
     except KeyboardInterrupt:
         input.driver.stop() #Might be needed for some input drivers, such as PiFaceCAD. 
         o.display_data("Does Ctrl+C", "hurt scripts?")
         sys.exit(1)
 
+def load_all_apps():
+    menu_contents = []
+    app_names = apps.module_names
+    for app_name in app_names:
+        print(app_name)
+        try:
+            app = load_app(app_name)
+            menu_contents.append([app.menu_name, app.callback])
+        except Exception as e:
+            o.display_data("Failed to import", app_name)
+            print(e)
+            sleep(3)
+            #raise
+    return menu_contents
+
+def load_app(name):
+    global app_list
+    app = importlib.import_module('apps.'+name+'.main')
+    app.init_app(i, o)
+    app_list[name] = app
+    return app    
+
 if __name__ == "__main__":
-    start()
+    parser = argparse.ArgumentParser(description="pyLCS runner")
+    parser.add_argument('-a', '--app', action="store", help="Launch pyLCS with a single app loaded (useful for testing)", default=None)
+    args = parser.parse_args()
+    launch(name=args.app)
