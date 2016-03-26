699a1fb154e09b9f0be5020b88b0794c38608391
Arsenijs <crimier@yandex.ru>
2016-03-26 16:16:25 +0200

    Added a warning to serial_lcd_0 driver

--- output/drivers/serial_lcd_0.py
+++ output/drivers/serial_lcd_0.py
@@ -2,6 +2,14 @@ from serial import Serial
 
 #ser_port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9CJVD59-if00-port0" #send that to settings or something something automagical
 
+"""
+ NOT WORKING
+ The first output driver I did. The idea was having an Arduino which would have a 16x2 screen connected,
+ take a string over serial, clear the screen, put first 16 characters on the first line, second 16 characters on the second line
+ and discard the rest.
+ Hey, it worked. I'm leaving this here in case you need an example or something.
+ Can't enable backlight, can't just clear the screen, is config-compatible and theoretically is even working. No Arduino code included.
+"""
 class Screen():
     """Class that has all the screen control functions and defines"""
     type = "char"
