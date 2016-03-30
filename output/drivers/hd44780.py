202fb47a1b8551870d156330bdb4451842215c9a
CRImier <crimier@yandex.ru>
2016-03-30 22:11:25 +0000

    Added a HD44780 library to reduce code size and modified MCP23008 LCD backpack library to work with HD44780 library

---
+++ output/drivers/hd44780.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python
+
+#
+# based on code from Adafruit, lrvick and LiquidCrystal
+# lrvick - https://github.com/lrvick/raspi-hd44780/blob/master/hd44780.py
+# LiquidCrystal - https://github.com/arduino/Arduino/blob/master/libraries/LiquidCrystal/LiquidCrystal.cpp
+# Adafruit - https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code
+#
+
+from time import sleep
+
+def delayMicroseconds(microseconds):
+    seconds = microseconds / float(1000000)  # divide microseconds by 1 million for seconds
+    sleep(seconds)
+
+def delay(milliseconds):
+    seconds = milliseconds / float(1000)  # divide microseconds by 1 million for seconds
+    sleep(seconds)
+
+
+class HD44780():
+
+    # commands
+    LCD_CLEARDISPLAY        = 0x01
+    LCD_RETURNHOME          = 0x02
+    LCD_ENTRYMODESET        = 0x04
+    LCD_DISPLAYCONTROL      = 0x08
+    LCD_CURSORSHIFT         = 0x10
+    LCD_FUNCTIONSET         = 0x20
+    LCD_SETCGRAMADDR        = 0x40
+    LCD_SETDDRAMADDR        = 0x80
+
+    # flags for display entry mode
+    LCD_ENTRYRIGHT          = 0x00
+    LCD_ENTRYLEFT           = 0x02
+    LCD_ENTRYSHIFTINCREMENT = 0x01
+    LCD_ENTRYSHIFTDECREMENT = 0x00
+
+    # flags for display on/off control
+    LCD_DISPLAYON           = 0x04
+    LCD_DISPLAYOFF          = 0x00
+    LCD_CURSORON            = 0x02
+    LCD_CURSOROFF           = 0x00
+    LCD_BLINKON             = 0x01
+    LCD_BLINKOFF            = 0x00
+
+    # flags for display/cursor shift
+    LCD_DISPLAYMOVE         = 0x08
+    LCD_CURSORMOVE          = 0x00
+
+    # flags for display/cursor shift
+    LCD_DISPLAYMOVE         = 0x08
+    LCD_CURSORMOVE          = 0x00
+    LCD_MOVERIGHT           = 0x04
+    LCD_MOVELEFT            = 0x00
+
+    # flags for function set
+    LCD_8BITMODE            = 0x10
+    LCD_4BITMODE            = 0x00
+    LCD_2LINE               = 0x08
+    LCD_1LINE               = 0x00
+    LCD_5x10DOTS            = 0x04
+    LCD_5x8DOTS             = 0x00
+
+    row_offsets = [0x00, 0x40, 0x14, 0x54]
+
+    display_function = LCD_FUNCTIONSET | LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS
+    displaycontrol = 0x0c
+    displaymode = 0x07
+
+    def __init__(self, cols=16, rows=2, debug=False):
+        self.cols = cols
+        self.rows = rows
+
+    def init_display(self):
+        self.write_byte(0x30)  # initialization
+        delay(20)
+        self.write_byte(0x30)  # initialization
+        delay(20)
+        self.write_byte(0x30)  # initialization
+        delay(20)
+        self.home()
+        self.write_byte(self.display_function)
+        self.write_byte(0x08)
+        self.display()
+        self.clear()
+
+    def display_data(self, *args):
+        self.clear()
+        args = args[:self.rows]
+        for line, arg in enumerate(args):
+            self.setCursor(line, 0)
+            self.println(arg[:self.cols].ljust(self.cols))
+
+    def println(self, line):
+        for char in line:
+            self.write_byte(ord(char), char_mode=True)     
+
+    def write_byte(self, byte, char_mode=False):
+        if self.debug and not char_mode:
+            print(hex(byte))
+        self.write4bits(byte >> 4, char_mode)
+        self.write4bits(byte & 0x0F, char_mode)
+
+    def home(self):
+        self.write_byte(self.LCD_RETURNHOME)  # set cursor position to zero
+        delayMicroseconds(3000)  # this command takes a long time!
+
+    def clear(self):
+        self.write_byte(self.LCD_CLEARDISPLAY)  # command to clear display
+        delayMicroseconds(3000)  # 3000 microsecond sleep, clearing the display takes a long time
+
+    def setCursor(self, row, col):
+        """ Set current input cursor to row and column specified """
+        self.write_byte(self.LCD_SETDDRAMADDR | (col + self.row_offsets[row]))
+
+    def noDisplay(self):
+        """ Turn the display off (quickly) """
+        self.displaycontrol &= ~self.LCD_DISPLAYON
+        self.write_byte(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+
+    def display(self):
+        """ Turn the display on (quickly) """
+        self.displaycontrol |= self.LCD_DISPLAYON
+        self.write_byte(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+
+    def noCursor(self):
+        """ Turns the underline cursor off """
+        self.displaycontrol &= ~self.LCD_CURSORON
+        self.write_byte(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+
+    def cursor(self):
+        """ Turns the underline cursor on """
+        self.displaycontrol |= self.LCD_CURSORON
+        self.write_byte(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+
+    def noBlink(self):
+        """ Turn the blinking cursor off """
+        self.displaycontrol &= ~self.LCD_BLINKON
+        self.write_byte(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+
+    def blink(self):
+        """ Turn the blinking cursor on """
+        self.displaycontrol |= self.LCD_BLINKON
+        self.write_byte(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+
+    def scrollDisplayLeft(self):
+        """ These commands scroll the display without changing the RAM """
+        self.write_byte(self.LCD_CURSORSHIFT | self.LCD_DISPLAYMOVE | self.LCD_MOVELEFT)
+
+    def scrollDisplayRight(self):
+        """ These commands scroll the display without changing the RAM """
+        self.write_byte(self.LCD_CURSORSHIFT | self.LCD_DISPLAYMOVE | self.LCD_MOVERIGHT)
+
+    def leftToRight(self):
+        """ This is for text that flows Left to Right """
+        self.displaymode |= self.LCD_ENTRYLEFT
+        self.write_byte(self.LCD_ENTRYMODESET | self.displaymode)
+
+    def rightToLeft(self):
+        """ This is for text that flows Right to Left """
+        self.displaymode &= ~self.LCD_ENTRYLEFT
+        self.write_byte(self.LCD_ENTRYMODESET | self.displaymode)
+
+    def autoscroll(self):
+        """ This will 'right justify' text from the cursor """
+        self.displaymode |= self.LCD_ENTRYSHIFTINCREMENT
+        self.write_byte(self.LCD_ENTRYMODESET | self.displaymode)
+
+    def noAutoscroll(self):
+        """ This will 'left justify' text from the cursor """
+        self.displaymode &= ~self.LCD_ENTRYSHIFTINCREMENT
+        self.write_byte(self.LCD_ENTRYMODESET | self.displaymode)
--- output/drivers/mcp23008.py
+++ output/drivers/mcp23008.py
@@ -7,22 +7,43 @@ def delay(time):
 def delayMicroseconds(time):
     sleep(time/1000000.0)
 
+from hd44780 import HD44780
 
-class Screen():
-    data_mask = 0x00
+class Screen(HD44780):
 
-    def __init__(self, rows=2, cols=16, addr=0x27):
-        self.rows = rows
-        self.cols = cols
-        self.bus = smbus.SMBus(1)
+    def __init__(self, rows=2, cols=16, addr=0x27, bus=1, debug=False):
+        HD44780.__init__(self, rows=rows, cols=cols, debug=debug)
+        self.bus_num = bus
+        self.bus = smbus.SMBus(self.bus_num)
         self.addr = addr
+        self.debug = debug
         self.i2c_init()
+        self.init_display()
         
+    def i2c_init(self):
+        self.setMCPreg(0x05, 0x0c)
+        self.setMCPreg(0x00, 0x00)
+        """delay(20.0)
+        self.write_byte(0x30)
+        delay(20.0)
+        self.write_byte(0x30)
+        delay(20.0)
+        self.write_byte(0x30)
+        delay(20.0)
+        self.write_byte(0x02)
+        delay(20.0)
+        for byte in [0x28, 0x08, 0x0c]:
+            self.write_byte(byte)
+        self.clear()"""
+
     def setMCPreg(self, reg, val):
         self.bus.write_byte_data(self.addr, reg, val)
 
-    def send2LCD(self, data):
-        data |= self.data_mask
+    def write4bits(self, data, char_mode=False):
+        if char_mode:
+            data |= 0x10
+        #if self.debug:
+        #    print(hex(data))
         self.setMCPreg(0x0a, data)
         data ^= 0x80
         delayMicroseconds(1.0)
@@ -32,68 +53,14 @@ class Screen():
         self.setMCPreg(0x0a, data)
         delay(1.0)
         
-    def writeLCDbyte(self, data):
-        self.send2LCD(data >> 4)
-        self.send2LCD(data & 0x0F)
-
-    def i2c_init(self):
-        self.setMCPreg(0x05, 0x0c)
-        self.setMCPreg(0x00, 0x00)
-        delay(20.0)
-        self.send2LCD(0x03)
-        delay(2.0)
-        self.send2LCD(0x03)
-        delayMicroseconds(100.0)
-        self.send2LCD(0x03)
-        delay(2.0)
-        self.send2LCD(0x02)
-        for byte in [0x28, 0x08, 0x0c]:
-            self.writeLCDbyte(byte)
-        delayMicroseconds(30.0)
-        self.writeLCDbyte(0x01)
-        delay(3)
-
-    def i2c_write(self, value):
-        self.bus.write_byte_data(self.addr, 0, value) 
-
-    def cursorTo(self, row, col):
-        offsets = [0x00, 0x40, 0x14, 0x54]
-        self.write_command(0x80|(col+offsets[row]))
-
-    def write_command(self, command):
-        self.writeLCDbyte(command)
-        delayMicroseconds(50.0)
-
-    def print_char(self, char):
-        self.data_mask |= 0x10
-        self.writeLCDbyte(ord(char))
-        self.data_mask ^= 0x10
-
-    def println(self, line):
-        for char in line:
-            self.print_char(char)
-
-    def clear(self):
-        self.write_command(0x01)
-
-    def display_data(self, *args):
-        self.clear()
-        for line, arg in enumerate(args):
-            self.cursorTo(line, 0)
-            self.println(arg[:self.cols].ljust(self.cols))
-       
 
 if __name__ == "__main__":
-    screen = Screen()
+    screen = Screen(bus=1, addr=0x27, cols=16, rows=2, debug=True)
+    print("Initialising with HD44780:")
     line = "0123456789012345"
-    while True:
-        screen.cursorTo(0, 0)
-        screen.println(line)
-        screen.cursorTo(1, 0)
-        screen.println(line)
-        sleep(1)      
-        screen.cursorTo(0, 0)
-        screen.println(line[::-1])
-        screen.cursorTo(1, 0)
-        screen.println(line[::-1])
-        sleep(1)      
+    if True:
+        screen.display_data(line, line[::-1])
+        sleep(1)
+        screen.display_data(line[::-1], line)
+        sleep(1)
+        screen.clear()
