44db0324f777a3eff49e0815a7e3b69d7b22e4de
CRImier <crimier@yandex.ru>
2016-04-12 15:43:17 +0300

    Added a TVservice app - not fully tested yet

---
+++ apps/raspberrypi/tvservice/main.py
@@ -0,0 +1,97 @@
+menu_name = "TVService"
+#Some globals for LCS
+callback = None
+#Some globals for us
+main_menu = None
+i = None
+o = None
+
+from time import sleep
+
+from ui import Menu, Printer, MenuExitException
+
+import tvservice
+
+def show_status():
+    try:
+        status = tvservice.status()
+    except IndexError, KeyError:
+        Printer(["Unknown mode", "open GH issue"], i, o)
+        return False
+    mode = status['mode']
+    status_menu_contents = [[["mode:", mode]]] #State is an element that's always there, let's see possible states
+    if mode == 'UNKNOWN':
+        Printer(["Unknown mode", "open GH issue"], i, o)
+        return False
+    if mode == 'NONE':
+        Printer(["No video out", "active"], i, o)
+        return False
+    #Adding elements that are there both in HDMI and TV mode
+    x, y = status["resolution"]
+    refresh_rate = status["refresh_rate"]
+    status_menu_contents.append([["Resolution&rate", "{}x{}, {}".format(x, y, refresh_rate)]])
+    if mode == 'HDMI':
+        group, mode, drive = status["gmd"]
+        status_menu_contents.append([["GroupModeDrive", "{}, {}, {}".format(group, mode, drive)]])
+    elif mode == 'TV':
+        status_menu_contents.append([["TV mode: ", status["tv_mode"]]])
+    status_menu_contents.append([["Ratio:", status["ratio"]]])
+    status_menu_contents.append(["Show flags", lambda: Printer(status["flags"], i, o, skippable=True)])
+    status_menu = Menu(status_menu_contents, i, o, "Tvservice status menu", entry_height=2)
+    status_menu.activate()
+
+def select_hdmi_mode():
+    menu_contents = []
+    try:
+        current_group = tvservice.status()['gmd'][0]
+    except KeyError, IndexError:
+        Printer(["Error, is HDMI", "enabled?"], i, o)
+        return False
+    modes = tvservice.get_modes(current_group)
+    for mode in modes:
+        width = mode["width"]
+        height = mode["height"]
+        rate = mode["rate"]
+        menu_contents.append(["{}x{}@{}".format(width, height, rate), lambda x=mode: set_mode(x)])
+    mode_menu = Menu(menu_contents, i, o, "Mode change menu")
+    mode_menu.activate()
+
+def set_mode(mode_desc):
+    group, mode, drive = tvservice.status()['gmd']
+    mode = mode_desc["code"]
+    tvservice.set_mode(group, mode, drive)
+    Printer(['Changed to', "{},{},{}".format(group, mode, drive)], i, o, skippable=True)
+    raise MenuExitException
+        
+def display_off():
+    tvservice.display_off()
+    Printer(['Disabled display'], i, o, skippable=True)
+        
+def display_on():
+    tvservice.display_on()
+    Printer(['Enabled display', 'with defaults'], i, o, skippable=True)
+        
+
+def launch():
+    try:
+       status = tvservice.status()
+    except OSError as e:
+       if e.errno == 2:
+           Printer(["Do you have", "tvservice?"], i, o, 3, skippable=True)
+           return
+       else:
+           raise e
+    else:
+        main_menu_contents = [
+        ["Status", show_status],
+        ["Choose HDMI mode", select_hdmi_mode],
+        ["Display on", display_on],
+        ["Display off", display_off]]
+        main_menu = Menu(main_menu_contents, i, o, "tvservice main menu")
+        main_menu.activate()
+
+
+def init_app(input, output):
+    global callback, i, o
+    i = input; o = output
+    callback = launch
