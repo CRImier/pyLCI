46f8a04edac43fbc8b78ba9d610ccc8a7111bd0d
CRImier <crimier@yandex.ru>
2016-04-20 23:37:28 +0300

    Made config_parse usable for reading any json files

--- helpers/config_parse.py
+++ helpers/config_parse.py
@@ -5,7 +5,7 @@ import json
 config_path = os.path.relpath("config.json")
 config = None
 
-def read_config():
+def read_config(config_path=config_path):
     try:
         f = open(config_path, 'r')
         data = json.load(f)
