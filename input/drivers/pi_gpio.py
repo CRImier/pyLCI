b83edf4d24cf1d590d77ff54584db288a1a3ad14
CRImier <crimier@yandex.ru>
2016-04-21 23:19:18 +0000

    Added __main__ section to pi_gpio, fixed some mistakes

--- input/drivers/pi_gpio.py
+++ input/drivers/pi_gpio.py
@@ -5,17 +5,17 @@ class InputDevice():
     """ A driver for pushbuttons attached to Raspberry Pi GPIO. It uses RPi.GPIO library. Button's first pin has to be attached to ground, second has to be attached to the GPIO pin and pulled up to 3.3V with a 1-10K resistor."""
 
     mapping = [
+    "KEY_UP",
+    "KEY_DOWN",
     "KEY_LEFT",
     "KEY_RIGHT",
     "KEY_KPENTER",
-    "KEY_HOME",
-    "KEY_END",
     "KEY_DELETE",
-    "KEY_UP",
-    "KEY_DOWN"]
+    "KEY_HOME",
+    "KEY_END"]
     stop_flag = False
     
-    def __init__(self, button_pins=[]):
+    def __init__(self, button_pins=[], debug=False):
         """Initialises the ``InputDevice`` object. 
 
         Kwargs:
@@ -23,7 +23,7 @@ class InputDevice():
         * ``button_pins``: GPIO mubers which to treat as buttons (GPIO.BCM numbering)
         """
         self.button_pins = button_pins
-        pass
+        self.debug = debug
 
     def start(self):
         """Polling loop. Stops when ``stop_flag`` is set to True."""
@@ -41,9 +41,12 @@ class InputDevice():
                     button_state = GPIO.input(pin_num)
                     if button_state != button_states[i]: # button state is changed
                         if button_state == True:
-                            print("Button {} released".format(i+1))
+                            if self.debug:
+                                print("Button {} released".format(i+1))
+                            pass
                         else:
-                            print("Button {} pressed".format(i+1))
+                            if self.debug:
+                                print("Button {} pressed".format(i+1))
                             key = self.mapping[i]
                             self.send_key(key)
                         button_states[i] = button_state
@@ -66,3 +69,8 @@ class InputDevice():
         self.thread = threading.Thread(target=self.start)
         self.thread.daemon = True
         self.thread.start()
+
+
+if __name__ == "__main__":
+    id = InputDevice(button_pins=[22, 23, 27, 24, 18, 4], debug=True)
+    id.start()
