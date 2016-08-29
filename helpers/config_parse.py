d201279719f13d634b7d5d0d8566b921a5ebf28f
CRImier <crimier.yandex.ru>
2016-08-29 23:41:48 +0300

    removed redundant try:catch in config helpers

--- helpers/config_parse.py
+++ helpers/config_parse.py
@@ -3,22 +3,13 @@ import os
 import json 
 
 def read_config(config_path):
-    try:
-        with open(config_path, 'r') as f:
-            data = json.load(f)
-    except (IOError, ValueError) as e:
-        raise
-    else:
-        return data
+    with open(config_path, 'r') as f:
+        data = json.load(f)
+    return data
      
 def write_config(config, config_path):
-    try:
-        with open(config_path, 'w') as f:
-            data = json.dump(config, f)
-    except (IOError, ValueError) as e:
-        raise
-    else:
-        return data
+    with open(config_path, 'w') as f:
+        json.dump(config, f)
      
 if __name__ == "__main__":
     config = read_config("../config.json")
