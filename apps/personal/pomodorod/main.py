e071d11bb367d7a46c7c8366a7bb95d481a58eaf
CRImier <crimier@yandex.ru>
2016-11-10 01:23:13 +0200

    PomodoroD app added

---
+++ apps/personal/pomodorod/main.py
@@ -0,0 +1,56 @@
+menu_name = "Pomodoro"
+
+i = None
+o = None
+
+from time import sleep
+
+from ui import Menu, MenuExitException, Printer, IntegerAdjustInput, Refresher, DialogBox
+
+from utils import RPCClient, RPCCommError
+server = RPCClient('localhost', 4515)
+
+def e_wr(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except RPCCommError:
+            Printer(["Can't connect", "to pomodorod!"], i, o, 1)
+    return wrapper
+
+def status_refresher_data():
+    try:
+        status_str = server.get_status()
+    except RPCCommError:
+        return ["Can't connect", "to pomodorod!"]
+    else:
+        return status_str
+
+def callback():
+    keymap = {"KEY_ENTER":pomodoro_options_menu, "KEY_KPENTER":pomodoro_options_menu}
+    refresher = Refresher(status_refresher_data, i, o, 1, keymap, "Pomodoro monitor")
+    refresher.activate()
+
+def start_monitoring():
+    server.start_work()
+    raise MenuExitException
+
+def stop_monitoring():
+    server.break_work()
+    raise MenuExitException
+
+def ack_notification():
+    server.acknowledge_notification()
+    raise MenuExitException
+
+def pomodoro_options_menu():
+    main_menu_contents = [
+    ["Acknowledge", e_wr(ack_notification)],
+    ["Start", e_wr(start_monitoring)],
+    ["Stop", e_wr(stop_monitoring)]]
+    main_menu = Menu(main_menu_contents, i, o, "Pomodoro options menu")
+    main_menu.activate()
+
+def init_app(input, output):
+    global i, o
+    i = input; o = output
