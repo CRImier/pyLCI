306397bdc6d540d98becc23e9f96baf89b4d438c
Arsenijs <crimier@yandex.ru>
2017-02-13 11:03:54 +0200

    Cleaned up Hello World app

--- apps/example_apps/test/main.py
+++ apps/example_apps/test/main.py
@@ -1,15 +1,17 @@
-menu_name = "Test app"
+menu_name = "Hello World" #pyLCI needs this variable to show app name in the main menu
 
-from ui import Printer
+from ui import Printer #This UI element prints your text on the screen, waits the amount of time you tell it to and exits.
+from ui import format_for_screen as ffs #This helper function takes a text message and splits it into blocks so that it fits the screen
 
-#Some globals for LCS
-callback = None
-#Some globals for us
+#Some globals for storing input and output device objects
 i = None
 o = None
 
-def init_app(input, output):
-    global callback, i, o
-    i = input; o = output
-    callback = lambda: Printer(["Hello and", "welcome to", "Aperture Science", "computer aided", "enrichment", "center"], i, o, sleep_time=5, skippable=True)
+def callback():
+    """A function that's called when the app is selected in the menu"""
+    Printer(ffs("Hello and welcome to Aperture Science computer aided enrichment center", o.cols), i, o, sleep_time=5, skippable=True)
 
+def init_app(input, output):
+    """A function called once when pyLCI loads, to pass all the variables to the app"""
+    global i, o
+    i = input; o = output
