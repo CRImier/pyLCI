3bd233665e8a706985562ada9fe27934b86d6642
CRImier <crimier@yandex.ru>
2017-04-17 03:24:46 +0300

    Added command status checks to emulator setup file

--- setup_emulator.sh
+++ setup_emulator.sh
@@ -1,6 +1,7 @@
 touch emulator
-sudo apt-get install python-pip
-sudo pip install luma.emulator
+sudo apt-get update
+sudo apt-get install python-pip || exit 1
+sudo pip install luma.emulator || exit 2
 cat << EOF > config.json
 {"input":[{"driver":"pygame_input"}],"output":[{"driver":"pygame_emulator"}]}
 EOF
