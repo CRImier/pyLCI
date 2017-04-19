a4a4a4ca2fd16131b0c8e34c8932d9a98659b02e
CRImier <crimier@yandex.ru>
2017-04-19 06:22:33 +0300

    UI element refactoring - most of work done, not tested thoroughly

--- ui/checkbox.py
+++ ui/checkbox.py
@@ -1,22 +1,14 @@
-from time import sleep
-from copy import copy
 import logging
 
-def to_be_foreground(func): #A safety check wrapper so that certain checks don't get called if menu is not the one active
-    def wrapper(self, *args, **kwargs):
-        if self.in_foreground:
-            return func(self, *args, **kwargs)
-        else:
-            return False
-    return wrapper
+from list_ui_base import BaseListUIElement, to_be_foreground
 
-class Checkbox():
+class Checkbox(BaseListUIElement):
     """Implements a checkbox which can be used to enable or disable some functions in your application. 
 
     Attributes:
 
     * ``contents``: list of checkbox elements which was passed either to ``Checkbox`` constructor or to ``checkbox.set_contents()``.
-       
+
       Checkbox element structure is a list, where:
          * ``element[0]`` (element's representation) is either a string, which simply has the element's value as it'll be displayed, such as "Menu element 1", or, in case of entry_height > 1, can be a list of strings, each of which represents a corresponding display row occupied by the element.
          * ``element[1]`` (element's name) is a name returned by the checkbox upon its exit in a dictionary along with its boolean value.
@@ -30,19 +22,13 @@ class Checkbox():
     * ``last_displayed_entry`` : Internal flag which points to the number of ``self._contents`` element which is at the lowest position of the checkbox menu as it's currently displayed on the screen
 
     """
-    contents = []
-    _contents = []
-    pointer = 0
-    in_foreground = False
-    name = ""
-    first_displayed_entry = 0
-    last_displayed_entry = None
     states = []
-    cancelled = False
+    accepted = True
+    exit_entry = ["Accept", None, 'accept']
 
-    def __init__(self, contents, i, o, name="Menu", entry_height=1, default_state=False, final_button_name="Save"):
+    def __init__(self, *args, **kwargs):
         """Initialises the Checkbox object.
-        
+
         Args:
 
             * ``contents``: a list of element descriptions, which can be constructed as described in the Checkbox object's docstring.
@@ -53,171 +39,45 @@ class Checkbox():
             * ``name``: Checkbox name which can be used internally and for debugging.
             * ``entry_height``: number of display rows one checkbox element occupies.
             * ``default_state``: default state of the element if not supplied.
+            * ``final_button_name``: name for the last button that confirms the selection
 
         """
-        self.i = i
-        self.o = o
-        self.entry_height = entry_height
-        self.final_button_name = final_button_name
-        self.name = name
-        self.set_contents(contents)
-        self.generate_keymap()
+        default_state = **kwargs.pop("default_state") if "default_state" in kwargs else False
+        final_button_name = **kwargs.pop("final_button_name") if "final_button_name" in kwargs else "Accept"
+        BaseListUIElement.__init__(self, *args, **kwargs)
+        self.exit_entry[0] = final_button_name
 
-    def to_foreground(self):
-        """ Is called when checkboxes ``activate()`` method is used, sets flags and performs all the actions so that checkbox can display its contents and receive keypresses. Also, updates the output device with rendered currently displayed checkbox elements."""
-        logging.info("checkbox {0} enabled".format(self.name))    
-        self.in_foreground = True
-        self.refresh()
-        self.set_keymap()
-
-    def activate(self):
-        """ A method which is called when checkbox needs to start operating. Is blocking, sets up input&output devices, renders the checkbox and waits until self.in_background is False, while checkbox callbacks are executed from the input device thread."""
-        logging.info("checkbox {0} activated".format(self.name))    
-        self.o.cursor()
-        self.to_foreground()
-        while self.in_foreground: #All the work is done in input callbacks
-            sleep(0.1)
-        self.o.noCursor()
-        logging.debug(self.name+" exited")
-        if self.cancelled:
-            return None
-        else:
+    def get_return_value(self):
+        if self.accepted:
             return {self._contents[index][1]:self.states[index] for index, element in enumerate(self._contents)}
+        else:
+            return None
 
     @to_be_foreground
-    def deactivate(self):
-        """ Deactivates the menu completely, exiting it. As for now, pointer state is preserved through checkbox activations/deactivations """
-        self.in_foreground = False
-        logging.info("checkbox {0} deactivated".format(self.name))    
-
-    @to_be_foreground
-    def cancel(self):
-        """ Deactivates the menu completely, exiting it. As for now, pointer state is preserved through checkbox activations/deactivations """
-        logging.info("checkbox {0} cancelled".format(self.name))    
-        self.cancelled = True
-        self.deactivate()
-
-    def print_contents(self):
-        """ A debug method. Useful for hooking up to an input event so that you can see the representation of checkbox's contents. """
-        logging.info(self._contents)
-
-    def print_name(self):
-        """ A debug method. Useful for hooking up to an input event so that you can see which UI element is currently processing input events. """
-        logging.info("Active menu is {0}".format(self.name))    
-
-    @to_be_foreground
-    def move_down(self):
-        """ Moves the pointer one element down, if possible. 
-        |Is typically used as a callback from input event processing thread.
-        |TODO: support going from bottom to top when pressing "down" with last checkbox element selected."""
-        if self.pointer < (len(self._contents)-1):
-            logging.debug("moved down")
-            self.pointer += 1  
-            self.refresh()    
-            return True
-        else: 
-            return False
-
-    @to_be_foreground
-    def move_up(self):
-        """ Moves the pointer one element up, if possible.
-        |Is typically used as a callback from input event processing thread.
-        |TODO: support going from top to bottom when pressing "up" with first checkbox element selected."""
-        if self.pointer != 0:
-            logging.debug("moved up")
-            self.pointer -= 1
-            self.refresh()
-            return True
-        else: 
-            return False
-
-    @to_be_foreground
-    def flip_state(self):
+    def select_entry(self):
         """ Changes the current element's state to the opposite.
         |Is typically used as a callback from input event processing thread. Afterwards, refreshes the screen."""
         logging.debug("element selected")
-        if len(self._contents) == 0:
+        if self._contents[self.pointer][2] == 'accept':
+            self.accepted = True
             self.deactivate()
-        else:
-            if self._contents[self.pointer][2] == 'exit':
-                self.deactivate()
-                return
-            self.states[self.pointer] = not self.states[self.pointer] #Just inverting.
-            self.refresh()
-                
-    def generate_keymap(self):
-        """Sets the keymap. In future, will allow per-system keycode-to-callback tweaking using a config file. """
-        keymap = {
-            "KEY_RIGHT":lambda: self.print_name(),
-            "KEY_LEFT":lambda: self.cancel(),
-            "KEY_UP":lambda: self.move_up(),
-            "KEY_DOWN":lambda: self.move_down(),
-            "KEY_KPENTER":lambda: self.flip_state(),
-            "KEY_ENTER":lambda: self.flip_state()
-            }
-        self.keymap = keymap
+            return
+        self.states[self.pointer] = not self.states[self.pointer] #Just inverting.
+        self.refresh()
 
-    def set_contents(self, contents):
-        """Sets the checkbox contents, as well as additionally re-sets ``last`` & ``first_displayed_entry`` pointers and calculates the value for ``last_displayed_entry`` pointer."""
-        self.contents = contents
-        self.process_contents(self.contents)
-        #Calculating the pointer to last element displayed
-        if len(self._contents) == 0:
-            self.last_displayed_entry = 0
-            self.first_displayed_entry = 0
-            return True
-        full_entries_shown = self.o.rows/self.entry_height
-        entry_count = len(self._contents)
-        self.first_displayed_entry = 0
-        if full_entries_shown > entry_count: #Display is capable of showing more entries than we have, so the last displayed entry is the last menu entry
-            #print("There are more display rows than entries can take, correcting")
-            self.last_displayed_entry = entry_count-1
-        else:
-            #print("There are no empty spaces on the display")
-            self.last_displayed_entry = full_entries_shown-1 #We start numbering elements with 0, so 4-row screen would show elements 0-3
-        #print("First displayed entry is {}".format(self.first_displayed_entry))
-        #print("Last displayed entry is {}".format(self.last_displayed_entry))
-        self.pointer = 0 #Resetting pointer to avoid confusions between changing menu contents
+    def validate_contents(self, contents):
+        pass
 
     def process_contents(self, contents):
-        """Processes contents for custom callbacks. Currently, only 'exit' calbacks are supported.
-
-        If ``self.append_exit`` is set, it goes through the menu and removes every callback which either is ``self.deactivate`` or is just a string 'exit'. 
-        |Then, it appends a single ["Exit", '', 'exit'] element at the end of checkbox contents. It makes dynamically appending elements to checkbox easier and makes sure there's only one "Exit" callback, at the bottom of the checkbox."""
-        self._contents = copy(contents)
-        self.states = [element[2] if len(element)>1 else self.default_state for element in copy(self._contents)]
-        self._contents.append([self.final_button_name, '', 'exit'])
+        self.states = [element[2] if len(element)>1 else self.default_state for element in contents]
+        self.contents.append(self.exit_entry)
         self.states.append(False) #For the final button, to maintain "len(states) == len(self._contents)"
         logging.debug("{}: menu contents processed".format(self.name))
 
-    @to_be_foreground
-    def set_keymap(self):
-        """Generate and sets the input device's keycode-to-callback mapping. Re-starts the input device because ofpassing-variables-between-threads issues."""
-        self.generate_keymap()
-        self.i.stop_listen()
-        self.i.clear_keymap()
-        self.i.keymap = self.keymap
-        self.i.listen()
-
     def get_displayed_data(self):
-        """Generates the displayed data in a way that the output device accepts. The output of this function can be fed in the o.display_data function.
-        |Corrects last&first_displayed_entry pointers if necessary, then gets the currently displayed elements' numbers, renders each one of them and concatenates them into one big list which it returns.
-        |Doesn't support partly-rendering entries yet."""
+        """Generates the displayed data in a way that the output device accepts.
+           The output of this function can be fed to the o.display_data function."""
         displayed_data = []
-        if len(self._contents) == 0:
-            return ["No checkbox entries"]
-        if self.pointer < self.first_displayed_entry:
-            #print("Pointer went too far to top, correcting")
-            self.last_displayed_entry -=  self.first_displayed_entry - self.pointer #The difference will mostly be 1 but I guess it might be more in case of concurrency issues
-            self.first_displayed_entry = self.pointer
-            #print("First displayed entry is {}".format(self.first_displayed_entry))
-            #print("Last displayed entry is {}".format(self.last_displayed_entry))
-        if self.pointer > self.last_displayed_entry:
-            #print("Pointer went too far to bottom, correcting")
-            self.first_displayed_entry += self.pointer - self.last_displayed_entry 
-            self.last_displayed_entry = self.pointer
-            #print("First displayed entry is {}".format(self.first_displayed_entry))
-            #print("Last displayed entry is {}".format(self.last_displayed_entry))
         disp_entry_positions = range(self.first_displayed_entry, self.last_displayed_entry+1)
         #print("Displayed entries: {}".format(disp_entry_positions))
         for entry_num in disp_entry_positions:
@@ -251,7 +111,7 @@ class Checkbox():
             if checked:
                 entry_content[0][0] == "*"
             else:
-                entry_content[0][0] == "*"
+                entry_content[0][0] == " "
             while len(entry_content) < self.entry_height: #Can't have less either, padding with empty strings if necessary
                 entry_content.append('')
             return entry_content
@@ -259,11 +119,3 @@ class Checkbox():
             raise Exception("Entries may contain either strings or lists of strings as their representations")
         #print("Rendered entry: {}".format(rendered_entry))
         return rendered_entry
-
-    @to_be_foreground
-    def refresh(self):
-        logging.debug("{0}: refreshed data on display".format(self.name))
-        displayed_data = self.get_displayed_data()
-        active_line_num = (self.pointer - self.first_displayed_entry)*self.entry_height
-        self.o.setCursor(active_line_num, 0)
-        self.o.display_data(*displayed_data)
--- ui/path_picker.py
+++ ui/path_picker.py
@@ -1,26 +1,16 @@
 import os
 import logging
-from time import sleep
-from threading import Event
 
-from menu import Menu, MenuExitException, to_be_foreground
+from list_ui_base import BaseListBackgroundableUIElement, to_be_foreground
+from menu import Menu, MenuExitException
 from printer import Printer
 
-class PathPicker(Menu):
-    """#Short description
-
-    """
-    no_entry_message = "Empty directory"
+class PathPicker(BaseListBackgroundableUIElement):
     path_chosen = None
-    exitable = True
-    append_exit = False
-    entry_height = 1
-    catch_exit = True
-    contents_hook = None
 
-    def __init__(self, path, i, o, callback = None, display_hidden = False, current_dot = False, prev_dot = True, scrolling=True):
+    def __init__(self, path, i, o, callback = None, display_hidden = False, current_dot = False, prev_dot = True, scrolling=True, **kwargs):
         """Initialises the Menu object.
-        
+
         Args:
 
             * ``path``: a path to start from.
@@ -34,67 +24,42 @@ class PathPicker(Menu):
             * ``display_hidden``: if set, FilePickerMenu displays hidden files.
 
         """
-        self.i = i
-        self.o = o
+        BaseListBackgroundableUIElement.__init__(self, [], i, o, entry_height=1, scrolling=True, append_exit=False)
         if not os.path.isdir(path):
              raise ValueError("PathPicker path has to be a directory!")
-        self.path = os.path.normpath(path)
-        self.name = "PathPickerMenu-{}".format(self.path)
         self.display_hidden = display_hidden
         self.callback = callback
         self.current_dot = current_dot
         self.prev_dot = prev_dot
-        self._in_background = Event()
-        self.set_contents([]) #Method inherited from Menu and needs an argument, but context is not right
-        self.generate_keymap()
-        self.scrolling={"enabled":scrolling,       
-                        "current_finished":False,  
-                        "current_scrollable":False,
-                        "counter":0,               
-                        "pointer":0}               
+        self.set_path(os.path.normpath(path))
+        self.update_keymap()
 
-    def activate(self):
-        """ A method which is called when menu needs to start operating. Is blocking, sets up input&output devices, renders the menu and waits until self.in_background is False, while menu callbacks are executed from the input device thread."""
-        logging.info("{0} activated".format(self.name))    
-        self.o.cursor()
-        self.to_foreground() 
-        while self.in_background: #All the work is done in input callbacks
-            sleep(0.1)
-            self.scroll()
-        self.o.noCursor()
-        logging.debug(self.name+" exited")
+    def get_return_value(self):
         return self.path_chosen
 
     @to_be_foreground
-    def select_element(self):
-        """ 
+    def select_entry(self):
+        """
         |Is typically used as a callback from input event processing thread.
         |After callback's execution is finished, sets the keymap again and refreshes the screen.
         |If menu has no elements, exits the menu.
         |If MenuExitException is returned from the callback, exits menu, too."""
         logging.debug("element selected")
-        if len(self._contents) > 0:
+        if len(self.contents) > 0:
             self.to_background()
-            self._contents[self.pointer][1]()
+            self.contents[self.pointer][1]()
             self.to_foreground()
             if self.path_chosen:
                 self.deactivate()
             else:
                 self.to_foreground()
 
-    def generate_keymap(self):
-        """Sets the keymap. In the future, will allow per-system keycode-to-callback tweaking using a config file. """
-        keymap = {
+    def update_keymap(self):
+        """Updates the keymap."""
+        self.keymap.update({
             "KEY_RIGHT":lambda: self.options_menu(),
-            "KEY_UP":lambda: self.move_up(),
-            "KEY_DOWN":lambda: self.move_down(),
-            "KEY_PAGEUP":lambda: self.page_up(),
-            "KEY_PAGEDOWN":lambda: self.page_down(),
-            "KEY_KPENTER":lambda: self.select_element(),
-            "KEY_ENTER":lambda: self.select_element(),
             "KEY_LEFT": lambda: self.go_back()
-            }
-        self.keymap = keymap
+        })
 
     def go_back(self):
         if self.path == '/':
@@ -103,14 +68,16 @@ class PathPicker(Menu):
         parent_path = os.path.split(self.path)[0]
         self.goto_dir(parent_path)
 
-    def process_contents(self):
-        self._contents = []
+    def set_path(self, path):
+        self.path = path
+        self.name = "PathPicker-{}".format(self.path)
+        contents = []
         self.pointer = 0
         if self.path != '/':
             if self.current_dot or self.prev_dot:
                 dot_path = os.path.join(self.path, '.')
-                if self.current_dot: self._contents.append(['.', lambda: self.goto_dir(dot_path)])
-                if self.prev_dot: self._contents.append(['..', lambda: self.goto_dir(dot_path+'.')])
+                if self.current_dot: contents.append(['.', lambda: self.goto_dir(dot_path)])
+                if self.prev_dot: contents.append(['..', lambda: self.goto_dir(dot_path+'.')])
         path_contents = os.listdir(self.path)
         files = []
         dirs = []
@@ -126,28 +93,28 @@ class PathPicker(Menu):
         files.sort()
         for dir in dirs:
             full_path = os.path.join(self.path, dir)
-            self._contents.append([dir, lambda x=full_path: self.goto_dir(x)])
+            contents.append([dir, lambda x=full_path: self.goto_dir(x)])
         for file in files:
             full_path = os.path.join(self.path, file)
-            self._contents.append([file, lambda x=full_path: self.select_path(x)])
+            contents.append([file, lambda x=full_path: self.select_path(x)])
+        self.set_contents(contents)
 
     @to_be_foreground
     def options_menu(self):
         self.to_background()
-        current_item = self._contents[self.pointer][0]
+        current_item = self.contents[self.pointer][0]
         full_path = os.path.join(self.path, current_item)
         contents = [["Select path",lambda x=full_path: self.option_select(x)],
                     ["See full name",lambda x=full_path: Printer(current_item, self.i, self.o)],
                     ["See full path",lambda x=full_path: Printer(x, self.i, self.o)],
                     ["Exit PathPicker", self.option_exit]]
         Menu(contents, self.i, self.o).activate()
-        self.o.cursor()
         if self.in_background:
             self.to_foreground()
 
     def option_exit(self):
         self.deactivate()
-        raise MenuExitException #Menu needs to exit when PathPicker exits. It doesn't, of course, as it's in the main loop, so the app's left hanging. 
+        raise MenuExitException #Menu needs to exit when PathPicker exits. It doesn't, of course, as it's in the main loop, so the app's left hanging.
         #One of the reasons MenuExitExceptions are there.
 
     def option_select(self, path):
@@ -155,18 +122,15 @@ class PathPicker(Menu):
         if self.callback is None:
             self.select_path(path)
             self.deactivate()
-            raise MenuExitException 
+            raise MenuExitException
         else:
             self.callback(path)
-            raise MenuExitException 
+            raise MenuExitException
 
     #@to_be_foreground
     def goto_dir(self, dir):
         dir = os.path.normpath(dir)
-        self.path = dir
-        self.name = "PathPicker-{}".format(self.path)
-        self.pointer = 0
-        self.set_contents([])
+        self.set_path(dir)
         self.refresh()
 
     #@to_be_foreground
@@ -174,7 +138,7 @@ class PathPicker(Menu):
         path = os.path.normpath(path)
         if self.callback is not None:
             self.to_background()
-            current_item = self._contents[self.pointer][0]
+            current_item = self.contents[self.pointer][0]
             full_path = os.path.join(self.path, current_item)
             self.callback(full_path)
             self.to_foreground()
