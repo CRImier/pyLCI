a9d5f9e0edfac71f0273da2ce964302c2e2fd643
CRImier <crimier@yandex.ru>
2017-05-29 13:10:36 +0300

    Removed Debian/Ubuntu-specific commands from emulator setup script

--- setup_emulator.sh
+++ setup_emulator.sh
@@ -1,6 +1,4 @@
 touch emulator
-sudo apt-get update
-sudo apt-get install python-pip || exit 1
 sudo pip install luma.emulator || exit 2
 cat << EOF > config.json
 {"input":[{"driver":"pygame_input"}],"output":[{"driver":"pygame_emulator"}]}
