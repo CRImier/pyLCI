01a8dd933bef15123c38824843fb01a76304794d
CRImier <crimier@yandex.ru>
2017-07-01 21:20:34 +0300

    Started adding I2C device read/write function to I2C tools app

--- apps/ee_apps/i2ctools/main.py
+++ apps/ee_apps/i2ctools/main.py
@@ -2,29 +2,38 @@ menu_name = "I2C tools"
 
 from subprocess import call
 from ui import Menu, Printer
-
+from collections import OrderedDict
 from time import sleep
 
 import smbus
 
+current_bus = None
+
 def i2c_detect():
-    o.clear()  #This code for printing data one element on one row begs for a separate UI element module
-    o.display_data("Scanning:")
-    bus = smbus.SMBus(1) # 1 indicates /dev/i2c-1
-    found_devices = []
+    global current_bus
+    Printer("Scanning:", i, o, 0)
+    current_bus = smbus.SMBus(1) # 1 indicates /dev/i2c-1
+    found_devices = OrderedDict()
     for device in range(128):
       try: #If you try to read and it answers, it's there
-         bus.read_byte(device)
-      except IOError: 
-         pass
+         current_bus.read_byte(device)
+      except IOError as e: 
+         if e.errno == 16:
+             found_devices[device] = "busy"
+         elif e.errno == 121:
+             pass
+         else:
+             print("Errno {} unknown - can be used? {}".format(e.errno, repr(e)))
       else:
-         found_devices.append(str(hex(device)))
-    
-    device_count = len(found_devices)
-    if device_count == 0:
-        Printer("No devices found", i, o, 2, skippable=True)
+         found_devices[device] = "ok"
+    if not found_devices:
+        Printer("No devices found", i, o, 2)
     else:
-        Printer(found_devices, i, o, 2, skippable=True)
+        data = ["{} - {}".format(hex(dev), state) for dev, state in found_devices.iteritems()]
+        Printer(data, i, o, 2)
+
+def i2c_device_menu(address):
+    current_bus
 
 #Some globals for LCS
 main_menu = None
@@ -34,13 +43,13 @@ i = None
 o = None
 
 main_menu_contents = [
-["I2C detect", i2c_detect],
-["Exit", 'exit']
+["I2C detect", i2c_detect]
+#["Input address", i2c_select]
 ]
 
 def init_app(input, output):
     global main_menu, callback, i, o
     i = input; o = output
-    main_menu = Menu(main_menu_contents, i, o, "I2C tools")
+    main_menu = Menu(main_menu_contents, i, o, "I2C tools menu")
     callback = main_menu.activate
 
