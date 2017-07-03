acd15e8da332ac338ca21ef0267ccc1354c60a8a
CRImier <crimier@yandex.ru>
2017-07-04 01:08:48 +0300

    Added phone app disable function in emulator setup script

--- setup_emulator.sh
+++ setup_emulator.sh
@@ -2,3 +2,5 @@ touch emulator
 cat << EOF > config.json
 {"input":[{"driver":"pygame_input"}],"output":[{"driver":"pygame_emulator"}]}
 EOF
+touch apps/phone/do_not_load #Phone app will try to connect to modem, emulator environment is unlikely to have one
+#In future, modem could be emulated - until that, the app will likely have to be disabled on emulators
