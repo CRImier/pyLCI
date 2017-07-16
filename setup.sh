574db782abaaa2692b8336869557add77d48b96e
CRImier <crimier@yandex.ru>
2017-07-17 02:19:35 +0300

    Modified setup.sh to enable one-script install on ZeroPhone

--- setup.sh
+++ setup.sh
@@ -4,12 +4,13 @@ if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 [ -f config.json ] || cp config.json.example config.json
-$SUDO apt-get install python python-pip
-#$SUDO mkdir -p $INSTALL_DIR
-#$SUDO cp ./. $INSTALL_DIR -R
-#cd $INSTALL_DIR
-#$SUDO cp pylci.service /etc/systemd/system/
-#$SUDO systemctl daemon-reload
-#$SUDO systemctl enable pylci.service
-#Start it maybe?
+$SUDO apt-get install python python-pip python-smbus
+$SUDO pip install luma.oled
+$SUDO mkdir -p $INSTALL_DIR
+$SUDO cp ./. $INSTALL_DIR -R
+cd $INSTALL_DIR
+$SUDO cp pylci.service /etc/systemd/system/
+$SUDO systemctl daemon-reload
+$SUDO systemctl enable pylci.service
+$SUDO systemctl start pylci.service
 
