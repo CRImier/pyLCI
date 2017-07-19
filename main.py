a99a931d7b054be1bfc6d61ebebc7182ecd8d539
Brian Dunlay <brian.dunlay@gmail.com>
2017-07-18 21:28:57 -0700

    -merges the refactor of main.py by 'Brian Dunlay'. -replaces pyLCI names by ZPUI

--- main.py
+++ main.py
@@ -1,128 +1,201 @@
-#!/usr/bin/env python2
-import sys
-import os
-from subprocess import call
-from time import sleep
+#!/usr/bin/env python
+
 import argparse
-#Welcome to ZPUI innards
-#Here, things are about i and o, which are input and output
-#And we output things for debugging, so o goes first.
-from output import output
-
-main_config_path = "/boot/zpui_config.json"
-backup_config_path = "./config.json"
-config_error_file = "/boot/zpui_config_error.repr"
-emulator_flag_filename = "emulator"
-
-#Debugging helper snippet
-import threading
-import traceback
+import logging
+import os
 import signal
 import sys
-def dumpthreads(*args):
-    print("")
-    print("SIGUSR received, dumping threads")
-    print("")
-    for th in threading.enumerate():
-        print(th)
-        traceback.print_stack(sys._current_frames()[th.ident])
-        print("")
-signal.signal(signal.SIGUSR1, dumpthreads)
+import threading
+import traceback
 
-#Getting ZPUI config, it will be passed to input and output initializers
-#If config at main_config_path exists, use that
-#If not, use the backup path
-#Also, log the errors to a file so that it can be debugged later
+from logging.handlers import RotatingFileHandler
+from subprocess import call
+from time import sleep
 
+from apps.app_manager import AppManager
 from helpers import read_config
-
-is_emulator = emulator_flag_filename in os.listdir(".")
-
-if not is_emulator:
-    try:
-        error_file = open(config_error_file, "w+")
-        config = read_config(main_config_path)
-    except Exception as e:
-        print(repr(e))
-        print("------------------------------")
-        print("Couldn't read main config, using backup config!")
-        error_file.write("Couldn't read main config: {}\n".format(repr(e)))
-        error_file.write("Using backup config!\n")
-        try:
-            config = read_config(backup_config_path)
-        except Exception as e:
-            print("Couldn't read backup config, exiting!")
-            error_file.write("Couldn't read backup config: {}\n".format(repr(e)))
-            error_file.write("Exiting!\n")
-            error_file.close()
-            sys.exit(1)
-    else:
-        if os.path.exists(config_error_file): os.remove(config_error_file)
-        error_file.close()
-else:
-    config = read_config(backup_config_path)
-
-
-output.init(config["output"])
-o = output.screen
+from input import input
+from output import output
+from splash import splash
 from ui import Printer, Menu
 
 
-try: #If there's an internal error, we show it on display and exit
-    from apps.app_manager import AppManager
-    #Now we init the input subsystem
-    from input import input
-    input.init(config["input"])
-    i = input.listener
-    if hasattr(o, "set_backlight_callback"):
-        o.set_backlight_callback(i)
-except:
-    Printer(["Oops. :(", "y u make mistake"], None, o, 0) #Yeah, that's about all the debug data. 
-    raise
+LOGGING_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'pylci.log'
+)
 
-def splash_screen():
+LOGGING_FMT = (
+    '[%(levelname)s] %(asctime)s %(filename)s [%(process)d]: %(message)s',
+    '%Y-%m-%d %H:%M:%S'
+)
+
+CONFIG_PATHS = [
+    '/boot/pylci_config.json',
+    './config.json'
+]
+
+
+def init():
+    """
+    Initialize input and output objects
+    """
+
+    config = None
+
+    # Load config
+    for path in CONFIG_PATHS:
+        try:
+            logging.debug('Loading config from {0}'.format(path))
+            config = read_config(path)
+        except:
+            logging.exception('Failed to load config')
+        else:
+            logging.debug('Successfully loaded config')
+            break
+
+    if config is None:
+        sys.exit('Failed to load any config file')
+
+    # Initialize output
     try:
-        from splash import splash
-        splash(i, o)
-    except ImportError:
-        pass
+        output.init(config['output'])
+        o = output.screen
+    except:
+        logging.exception('Failed to initialize the output object')
+        raise
 
-def exception_wrapper(callback):
-    """This is a wrapper for all applications and menus. It catches exceptions and stops the system the right way when something bad happens, be that a Ctrl+c or an exception in one of the applications."""
+    # Initialize input
+    try:
+        # Now we can show errors on the display
+        input.init(config['input'])
+        i = input.listener
+    except:
+        logging.exception('Failed to initialize the input object')
+        Printer(['Oops. :(', 'y u make mistake'], None, o, 0)
+        raise
+
+    return i, o
+
+
+def launch(name=None, **kwargs):
+    """
+    Launches pyLCI, either in full mode or in
+    single-app mode (if ``name`` kwarg is passed).
+    """
+
+    i, o = init()
+    app_man = AppManager('apps', Menu, Printer, i, o)
+
+    if name is None:
+        try:
+            splash(i, o)
+        except:
+            logging.exception('Failed to load the splash screen')
+
+        # Load all apps
+        app_menu = app_man.load_all_apps()
+        app_entry = app_menu.activate
+    else:
+        # If using autocompletion from main folder, it might
+        # append a / at the name end, which isn't acceptable
+        # for load_app
+        name = name.rstrip('/')
+
+        # Load only single app
+        try:
+            app = app_man.load_app(name)
+        except:
+            logging.exception('Failed to load the app: {0}'.format(name))
+            i.atexit()
+            raise
+        app_entry = app.callback
+
+    exception_wrapper(app_entry, i, o)
+
+
+def exception_wrapper(callback, i, o):
+    """
+    This is a wrapper for all applications and menus.
+    It catches exceptions and stops the system the right
+    way when something bad happens, be that a Ctrl+c or
+    an exception in one of the applications.
+    """
+
     try:
         callback()
     except KeyboardInterrupt:
+        logging.info('Caught KeyboardInterrupt')
         Printer(["Does Ctrl+C", "hurt scripts?"], None, o, 0)
         i.atexit()
         sys.exit(1)
-    except Exception as e:
-        traceback.print_exc()
+    except:
+        logging.exception('A wild exception appears!')
         Printer(["A wild exception", "appears!"], None, o, 0)
         i.atexit()
         sys.exit(1)
     else:
-        Printer("Exiting ZPUI", None, o, 0)
+        logging.info('Exiting pyLCI')
+        Printer("Exiting pyLCI", None, o, 0)
         i.atexit()
         sys.exit(0)
 
-def launch(name=None):
-    """Function that launches ZPUI, either in full mode or single-app mode (if ``name`` kwarg is passed)."""
-    app_man = AppManager("apps", i, o)
-    if name != None:
-        name = name.rstrip('/') #If using autocompletion from main folder, it might append a / at the name end, which isn't acceptable for load_app
-        try:
-            app = app_man.load_app(name)
-        except:
-            i.atexit()
-            raise
-        exception_wrapper(app.on_start if hasattr(app, "on_start") else app.callback)
-    else:
-        splash_screen()
-        app_menu = app_man.load_all_apps()
-        exception_wrapper(app_menu.activate)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="ZPUI runner")
-    parser.add_argument('-a', '--app', action="store", help="Launch ZPUI with a single app loaded (useful for testing)", default=None)
+def dumpthreads(*args):
+    """
+    Helpful signal handler for debugging threads
+    """
+
+    print('\nSIGUSR received, dumping threads\n')
+    for th in threading.enumerate():
+        print(th)
+        traceback.print_stack(sys._current_frames()[th.ident])
+        print('')
+
+
+if __name__ == '__main__':
+    """
+    Parses arguments, initializes logging, launches pyLCI
+    """
+
+    # Signal handler for debugging
+    signal.signal(signal.SIGUSR1, dumpthreads)
+
+    # Setup argument parsing
+    parser = argparse.ArgumentParser(description='pyLCI runner')
+    parser.add_argument(
+        '--app',
+        '-a',
+        help='Launch pyLCI with a single app loaded (useful for testing)',
+        dest='name',
+        default=None)
+    parser.add_argument(
+        '--log-level',
+        '-l',
+        help='The minimum log level to output',
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        default='INFO')
     args = parser.parse_args()
-    launch(name=args.app)
+
+    # Setup logging
+    logger = logging.getLogger()
+    formatter = logging.Formatter(*LOGGING_FMT)
+
+    # Rotating file logs (for debugging crashes)
+    rotating_handler = RotatingFileHandler(
+        LOGGING_PATH,
+        maxBytes=10000,
+        backupCount=5)
+    rotating_handler.setFormatter(formatter)
+    logger.addHandler(rotating_handler)
+
+    # Live console logging
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # Set log level
+    logger.setLevel(args.log_level)
+
+    # Launch pyLCI
+    launch(**vars(args))
