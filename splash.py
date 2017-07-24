8639a886bde4ecb2888a3c4f599e56a70d80b4c9
Brian Dunlay <brian.dunlay@gmail.com>
2017-07-23 22:16:26 -0700

    added attribute check to splash

--- splash.py
+++ splash.py
@@ -6,11 +6,11 @@ from time import sleep
 def splash(i, o):
     image = PIL.Image.open("splash.png").convert('L')
     image = invert(image)
+
     #FIXME:
-    # need a first-class API call on the display to do this
+    # need a first-class API call on the display to do this properly
     # Moving the emulator out of proc broke this feature (can't access
-    # the 'device' property anymore
-
-#    image = image.convert(o.device.mode)
-#    o.device.display(image)
-
+    # the 'device' property anymore)
+    if hasattr(o, "device"):
+        image = image.convert(o.device.mode)
+        o.device.display(image)
