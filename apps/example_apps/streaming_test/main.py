d801277ffc6e95a9533a104efbb68c9c8a7560a9
CRImier <crimier@yandex.ru>
2017-07-27 02:53:28 +0300

    Improved key test app

--- apps/example_apps/streaming_test/main.py
+++ apps/example_apps/streaming_test/main.py
@@ -1,6 +1,6 @@
 menu_name = "Refresher app" 
 
-from ui import Refresher
+from ui import Refresher, Printer, format_for_screen as ffs
 
 counter = 0
 keys_called = []
@@ -11,7 +11,12 @@ def process_key(key, *args):
         keys_called = keys_called[1:]
     keys_called.append([counter, key])
     counter += 1
-    refresher.refresh() #Makes changes appear faster
+    last_three_keys = [k for c, k in keys_called][-3:]
+    keys_are_left = [k=="KEY_LEFT" for k in last_three_keys]
+    if len(last_three_keys) >= 3 and all(keys_are_left):
+        refresher.deactivate()
+    else:
+        refresher.refresh() #Makes changes appear faster
 
 def get_keys():
     return ["{}:{}".format(num, key) for num, key in keys_called]
@@ -26,5 +31,7 @@ def init_app(input, output):
     i = input; o = output 
     i.set_streaming(process_key)
     refresher = Refresher(get_keys, i, o, 1, name="Key monitor")
+    refresher.keymap.pop("KEY_LEFT") #Removing deactivate callback to show KEY_LEFT
+    Printer(ffs("To exit this app, press LEFT 3 times", o.cols), i, o)
     callback = refresher.activate
 
