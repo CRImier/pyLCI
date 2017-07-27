8a09423d01e26b46ad3689a22a68ea2c493c4f9e
CRImier <crimier@yandex.ru>
2017-07-27 03:50:00 +0300

    Improved key test app callback

--- apps/example_apps/streaming_test/main.py
+++ apps/example_apps/streaming_test/main.py
@@ -1,6 +1,6 @@
 menu_name = "Refresher app" 
 
-from ui import Refresher, Printer, format_for_screen as ffs
+from ui import Refresher, PrettyPrinter
 
 counter = 0
 keys_called = []
@@ -11,9 +11,8 @@ def process_key(key, *args):
         keys_called = keys_called[1:]
     keys_called.append([counter, key])
     counter += 1
-    last_three_keys = [k for c, k in keys_called][-3:]
-    keys_are_left = [k=="KEY_LEFT" for k in last_three_keys]
-    if len(last_three_keys) >= 3 and all(keys_are_left):
+    ltk = [k for c, k in keys_called][-3:]
+    if len(ltk) >= 3 and ltk[0]==ltk[1]==ltk[2]:
         refresher.deactivate()
     else:
         refresher.refresh() #Makes changes appear faster
@@ -22,16 +21,16 @@ def get_keys():
     return ["{}:{}".format(num, key) for num, key in keys_called]
 
 refresher = None
-callback = None
 i = None #Input device
 o = None #Output device
 
 def init_app(input, output):
-    global refresher, callback, i, o
+    global i, o
     i = input; o = output 
+
+def callback():
+    global refresher
     i.set_streaming(process_key)
     refresher = Refresher(get_keys, i, o, 1, name="Key monitor")
     refresher.keymap.pop("KEY_LEFT") #Removing deactivate callback to show KEY_LEFT
-    Printer(ffs("To exit this app, press LEFT 3 times", o.cols), i, o)
-    callback = refresher.activate
-
+    PrettyPrinter("To exit this app, press the same key 3 times", i, o)
