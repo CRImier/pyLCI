14326816fc1380fd2f6b1e08de63e5536be6ee8b
CRImier <crimier@yandex.ru>
2017-10-16 04:39:55 +0300

    Added sample callback to file browser app

--- apps/utils/file_browser/main.py
+++ apps/utils/file_browser/main.py
@@ -1,6 +1,7 @@
 menu_name = "File browser" 
 
-from ui import PathPicker
+from ui import PathPicker, Printer
+import os
 
 #Callback global for pyLCI. It gets called when application is activated in the main menu
 callback = None
@@ -14,6 +15,14 @@ def init_app(input, output):
     i = input; o = output #Getting references to output and input device objects and saving them as globals
     callback = browse
 
+def print_path(path):
+    if os.path.isdir(path):
+        Printer("Dir: {}".format(path), i, o, 5)
+    elif os.path.isfile(path):
+        Printer("File: {}".format(path), i, o, 5)
+    else:
+        Printer("WTF: {}".format(path), i, o, 5) # ;-P
+
 def browse():
-    path_picker = PathPicker("/", i, o)
+    path_picker = PathPicker("/", i, o, callback=)
     path_picker.activate() #Menu yet to be added
