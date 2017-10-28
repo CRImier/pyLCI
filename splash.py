584c97405203d5f1bdfbfd48d477fda70ccfe14f
CRImier <crimier@yandex.ru>
2017-10-28 08:21:54 +0300

    Simplified splash screen

--- splash.py
+++ splash.py
@@ -1,16 +1,8 @@
-import PIL
-from PIL.ImageOps import invert
-from luma.core.render import canvas
 from time import sleep
+import PIL
 
 def splash(i, o):
     image = PIL.Image.open("splash.png").convert('L')
-    image = invert(image)
-
-    #FIXME:
-    # need a first-class API call on the display to do this properly
-    # Moving the emulator out of proc broke this feature (can't access
-    # the 'device' property anymore)
-    if hasattr(o, "device"):
-        image = image.convert(o.device.mode)
-        o.device.display(image)
+    image = PIL.ImageOps.invert(image)
+    image = image.convert(o.device.mode)
+    o.display_image(image)
