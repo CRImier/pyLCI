eaedc5e6ad906cf20d810445a74cb7610142d81d
CRImier <crimier@yandex.ru>
2017-10-28 11:36:14 +0300

    Prettifying lsusb app

--- apps/system_apps/lsusb/main.py
+++ apps/system_apps/lsusb/main.py
@@ -1,6 +1,6 @@
 menu_name = "USB devices"
 
-from ui import Menu, Printer, ellipsize
+from ui import Menu, Printer, PrettyPrinter, ellipsize
 
 import lsusb
 
@@ -9,11 +9,12 @@ def show_devices():
     try:
         usb_devices = lsusb.lsusb()
     except OSError:
-        Printer(["Do you have", "lsusb?"], i, o, 2)
+        PrettyPrinter("Do you have lsusb?", i, o, 2)
         return False
     for bus, dev, vid_pid, name in usb_devices:   
         ell_name = ellipsize(name, o.cols)
-        menu_contents.append([["{}{},{}".format(bus, dev, vid_pid), ell_name], lambda x=name: Printer(x, i, o, skippable=True)])
+        info = "{}\n{}".format(vid_pid, name)
+        menu_contents.append([["D{},B{},{}".format(bus, dev, vid_pid), ell_name], lambda x=info: PrettyPrinter(x, i, o, skippable=True)])
     Menu(menu_contents, i, o, entry_height=2).activate()
 
 #Some globals for pyLCI
