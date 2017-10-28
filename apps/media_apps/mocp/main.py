d99ae658f354f7e6bc69046cadad4267410f61ab
CRImier <crimier@yandex.ru>
2017-10-29 00:07:12 +0300

    Adding shuffle, repeat and autnoext control to mocp app

--- apps/media_apps/mocp/main.py
+++ apps/media_apps/mocp/main.py
@@ -1,12 +1,9 @@
 menu_name = "MOCP control"
 
 from subprocess import call
-from ui import Menu, Printer
+from ui import Menu, Printer, DialogBox
 
-#Some globals for LCS
-main_menu = None
 callback = None
-#Some globals for us
 i = None
 o = None
 
@@ -26,11 +23,27 @@ def mocp_next():
 def mocp_prev():
     mocp_command("-r")
 
+def option_switch_dialog(option):
+    answer = DialogBox([["On", 'o'], ["Off", 'u'], ["Toggle", "t"]], i, o, message=option.capitalize()+":", name="MOCP {} option control dialog".format(option)).activate()
+    if answer: mocp_switch_option(answer, option)
+
+shuffle_dialog = lambda: option_switch_dialog("shuffle")
+repeat_dialog = lambda: option_switch_dialog("repeat")
+autonext_dialog = lambda: option_switch_dialog("autonext")
+
+def mocp_switch_option(switch_type, option):
+    mocp_command("-{}".format(switch_type), option)
+
+def mocp_prev():
+    mocp_command("-r")
+
 main_menu_contents = [ 
 ["Toggle play/pause", mocp_toggle_play],
 ["Next song", mocp_next],
 ["Previous song", mocp_prev],
-["Exit", 'exit']
+["Shuffle", shuffle_dialog],
+["Repeat", repeat_dialog],
+["Autonext", autonext_dialog]
 ]
 
 def init_app(input, output):
