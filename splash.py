8fa92741abbdb42221c1bec0ff3ba6ca8c7effba
CRImier <crimier@yandex.ru>
2017-11-06 21:17:03 +0200

    Something weird with splash screen, fixing

--- splash.py
+++ splash.py
@@ -1,8 +1,9 @@
 from time import sleep
+from PIL import ImageOps
 import PIL
 
 def splash(i, o):
     image = PIL.Image.open("splash.png").convert('L')
-    image = PIL.ImageOps.invert(image)
+    image = ImageOps.invert(image)
     image = image.convert(o.device.mode)
     o.display_image(image)
