d8765dc566fb3ef57417ebf1c2ef097e84ca223e
monsieur_h <monsieur_h@riseup.net>
2017-12-02 21:11:23 +0100

    Replaces print by logger calls. Uncomments locking function identification mechanism

--- ui/utils.py
+++ ui/utils.py
@@ -1,4 +1,8 @@
+import logging
 from functools import wraps
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def to_be_foreground(func):
@@ -24,14 +28,20 @@ def check_value_lock(func):
     def wrapper(self, *args, **kwargs):
         assert self.value_lock, "Class has no member self.value_lock"  # todo:maybe we should create it here ?
         # Value-changing code likely to run in concurrent thread and therefore we need a lock
-        # if self.__locked_name__ is not None: print("Another function already locked the thread! Name is {}, current is {}".format(self.__locked_name__, func.__name__))
+        if self.__locked_name__ is not None:
+            logger.warning(
+                "Another function already locked the thread! Name is {}, current is {}".format(
+                    self.__locked_name__,
+                    func.__name__
+                )
+            )
         self.value_lock.acquire()
-        # self.__locked_name__ = func.__name__
-        # print("Locked function {}".format(func.__name__))
+        self.__locked_name__ = func.__name__
+        logger.debug("Locked function {}".format(func.__name__))
         result = func(self, *args, **kwargs)
         self.value_lock.release()
-        # print("Unlocked function {}".format(func.__name__))
-        # self.__locked_name__ = None
+        logger.debug("Unlocked function {}".format(func.__name__))
+        self.__locked_name__ = None
         return result
 
     return wrapper
