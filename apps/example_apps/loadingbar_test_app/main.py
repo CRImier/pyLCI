efd22319beaba84b7071e2e8675c9ecfa396c3ea
CRImier <crimier@yandex.ru>
2017-12-08 20:35:31 +0200

    Modified the LoadingIndicator text app to 1) be able to dynamically pick the loading bar to be tested 2) run the loading bars in background, so that they'd be tested automatically

--- apps/example_apps/loadingbar_test_app/main.py
+++ apps/example_apps/loadingbar_test_app/main.py
@@ -1,21 +1,41 @@
+from time import sleep
 from apps.zero_app import ZeroApp
-from ui.loading_indicators import ProgressBar
-
+from ui import ProgressBar, DottedProgressIndicator, Listbox
 
 class LoadingBarExampleApp(ZeroApp):
     def __init__(self, i, o):
         super(LoadingBarExampleApp, self).__init__(i, o)
-        self.menu_name = "Loading test APP"
+        self.menu_name = "Loading bar test app"
 
-        self.loading_screen = ProgressBar(self.i, self.o, refresh_interval=.1, show_percentage=True)
-        # todo: uncomment line below to test another indicator
-        # self.loading_screen = DottedProgressIndicator(self.i, self.o, refresh_interval=.1, show_percentage=True)
+        self.progress_bar = ProgressBar(self.i, self.o,
+                                          refresh_interval=.1,
+                                          show_percentage=True,
+                                          percentage_offset=0,
+                                          keymap={"KEY_RIGHT":self.increase_progress})
 
-        self.loading_screen.keymap['KEY_RIGHT'] = self.increase_progress
+        self.dotted_progress_bar = DottedProgressIndicator(self.i, self.o,
+                                                       refresh_interval=.1)
+
+        lb_contents = [["Progress bar", self.progress_bar],
+                       ["Dotted bar", self.dotted_progress_bar]]
+        self.bar_choice_listbox = Listbox(lb_contents, self.i, self.o)
 
     def increase_progress(self):
         self.loading_screen.progress += .05
 
     def on_start(self):
         super(LoadingBarExampleApp, self).on_start()
-        self.loading_screen.activate()
+        chosen_loading_bar = self.bar_choice_listbox.activate()
+        chosen_loading_bar.run_in_background()
+        if isinstance(chosen_loading_bar, ProgressBar):
+            for i in range(101):
+                chosen_loading_bar.progress = float(i)/100
+                sleep(0.1)
+            sleep(2)
+            for i in range(101)[::-1]:
+                chosen_loading_bar.progress = float(i)/100
+                sleep(0.1)
+        else:
+            sleep(3)
+            chosen_loading_bar.deactivate()
+
