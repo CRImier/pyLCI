ad8d69c96e00edc2f9fb84a9a547a052f7ef6c24
CRImier <crimier@yandex.ru>
2017-12-09 22:25:49 +0200

    Adding game logic files for 2048 game, providing attribution

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ZPUI
 Default ZeroPhone UI framework (based on [pyLCI](pylci.rtfd.io))
 
+ZPUI is licensed under Apache 2.0 license, with 3rd party components under MIT - see LICENSE and LICENSE-3RD-PARTY for details
+
 [Project description on Hackaday](https://hackaday.io/project/19035)
 
 <!---[Project documentation](http://zpui.readthedocs.org/en/latest/)
---
+++ apps/games/g_2048/logic.py
@@ -0,0 +1,146 @@
+"""The MIT License (MIT)
+
+Copyright (c) 2014 Tay Yang Shun
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
+from random import *
+
+def new_game(n):
+    matrix = []
+
+    for i in range(n):
+        matrix.append([0] * n)
+    return matrix
+
+def add_two(mat):
+    a=randint(0,len(mat)-1)
+    b=randint(0,len(mat)-1)
+    while(mat[a][b]!=0):
+        a=randint(0,len(mat)-1)
+        b=randint(0,len(mat)-1)
+    mat[a][b]=2
+    return mat
+
+
+def game_state(mat):
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if mat[i][j]==2048:
+                return 'win'
+    for i in range(len(mat)-1): #intentionally reduced to check the row on the right and below
+        for j in range(len(mat[0])-1): #more elegant to use exceptions but most likely this will be their solution
+            if mat[i][j]==mat[i+1][j] or mat[i][j+1]==mat[i][j]:
+                return 'not over'
+    for i in range(len(mat)): #check for any zero entries
+        for j in range(len(mat[0])):
+            if mat[i][j]==0:
+                return 'not over'
+    for k in range(len(mat)-1): #to check the left/right entries on the last row
+        if mat[len(mat)-1][k]==mat[len(mat)-1][k+1]:
+            return 'not over'
+    for j in range(len(mat)-1): #check up/down entries on last column
+        if mat[j][len(mat)-1]==mat[j+1][len(mat)-1]:
+            return 'not over'
+    return 'lose'
+
+def reverse(mat):
+    new=[]
+    for i in range(len(mat)):
+        new.append([])
+        for j in range(len(mat[0])):
+            new[i].append(mat[i][len(mat[0])-j-1])
+    return new
+
+def transpose(mat):
+    new=[]
+    for i in range(len(mat[0])):
+        new.append([])
+        for j in range(len(mat)):
+            new[i].append(mat[j][i])
+    return new
+
+def cover_up(mat):
+    new=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    done=False
+    for i in range(4):
+        count=0
+        for j in range(4):
+            if mat[i][j]!=0:
+                new[i][count]=mat[i][j]
+                if j!=count:
+                    done=True
+                count+=1
+    return (new,done)
+
+def merge(mat):
+    done=False
+    for i in range(4):
+         for j in range(3):
+             if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
+                 mat[i][j]*=2
+                 mat[i][j+1]=0
+                 done=True
+    return (mat,done)
+
+
+def up(game):
+        print("up")
+        # return matrix after shifting up
+        game=transpose(game)
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        game=transpose(game)
+        return (game,done)
+
+def down(game):
+        print("down")
+        game=reverse(transpose(game))
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        game=transpose(reverse(game))
+        return (game,done)
+
+def left(game):
+        print("left")
+        # return matrix after shifting left
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        return (game,done)
+
+def right(game):
+        print("right")
+        # return matrix after shifting right
+        game=reverse(game)
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        game=reverse(game)
+        return (game,done)
