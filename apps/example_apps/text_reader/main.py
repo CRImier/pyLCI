5ce08261856acd3f237a54735a2abcf250007714
monsieur_h <ray.hubert@riseup.net>
2017-12-10 22:54:55 +0100

    weird broken link

--- apps/example_apps/text_reader/main.py
+++ apps/example_apps/text_reader/main.py
@@ -22,7 +22,7 @@ In the face of ambiguity, refuse the temptation to guess.
 There should be one—and preferably only one—obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
 Now is better than never.
-Although never is often better than right now.[5]
+Although never is often better than right now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea—let's do more of those!
