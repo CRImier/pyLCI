9ed35f3ad50d353baa2ead5fe53982fd40917d37
monsieur_h <ray.hubert@riseup.net>
2017-12-14 12:44:20 +0100

    adds Singleton class to helpers.general

--- helpers/general.py
+++ helpers/general.py
@@ -30,3 +30,13 @@ def flatten(foo):
                 yield y
         else:
             yield x
+
+
+# noinspection PyTypeChecker,PyArgumentList
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
