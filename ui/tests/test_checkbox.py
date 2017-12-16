32a7180bc9462498a6c1d66d858209d1bddce48a
monsieur_h <ray.hubert@riseup.net>
2017-12-16 23:54:39 +0100

    fixes some formatting in tests

--- ui/tests/test_checkbox.py
+++ ui/tests/test_checkbox.py
@@ -5,11 +5,11 @@ import unittest
 
 from mock import patch, Mock
 
-os.sys.path.append(os.path.dirname(os.path.abspath('.')))
-
 from ui import Checkbox
 
-#set up logging
+os.sys.path.append(os.path.dirname(os.path.abspath('.')))
+
+# set up logging
 LOG_FORMAT = '%(levelname)s %(asctime)-15s %(name)s  %(message)s'
 logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,12 +18,15 @@ logger = logging.getLogger(__name__)
 def get_mock_input():
     return Mock()
 
+
 def get_mock_output(rows=2, cols=16):
     m = Mock()
     m.configure_mock(rows=rows, cols=cols, type=["char"])
     return m
 
+
 cb_name = "Test checkbox"
+
 
 class TestCheckbox(unittest.TestCase):
     """tests dialog box class"""
@@ -44,58 +47,62 @@ class TestCheckbox(unittest.TestCase):
         """tests whether the exit label of one Checkbox leaks into another"""
         i = get_mock_input()
         o = get_mock_output()
-        c1 = Checkbox([["a", "1"]], i, o, name=cb_name+"1", final_button_name = "Name1")
-        c2 = Checkbox([["b", "2"]], i, o, name=cb_name+"2", final_button_name = "Name2")
-        c3 = Checkbox([["c", "3"]], i, o, name=cb_name+"3")
-        assert(c1.exit_entry != c2.exit_entry)
-        assert(c2.exit_entry != c3.exit_entry)
-        assert(c1.exit_entry != c3.exit_entry)
+        c1 = Checkbox([["a", "1"]], i, o, name=cb_name + "1", final_button_name="Name1")
+        c2 = Checkbox([["b", "2"]], i, o, name=cb_name + "2", final_button_name="Name2")
+        c3 = Checkbox([["c", "3"]], i, o, name=cb_name + "3")
+        assert (c1.exit_entry != c2.exit_entry)
+        assert (c2.exit_entry != c3.exit_entry)
+        assert (c1.exit_entry != c3.exit_entry)
 
     def test_left_key_returns_none(self):
         num_elements = 3
-        contents = [["A"+str(i), "a"+str(i)] for i in range(num_elements)]
+        contents = [["A" + str(i), "a" + str(i)] for i in range(num_elements)]
         cb = Checkbox(contents, get_mock_input(), get_mock_output(), name=cb_name)
         cb.refresh = lambda *args, **kwargs: None
 
-        #Checking at the start of the list
+        # Checking at the start of the list
         def scenario():
-            cb.deactivate() #KEY_LEFT
+            cb.deactivate()  # KEY_LEFT
             assert not cb.in_foreground
+
         with patch.object(cb, 'idle_loop', side_effect=scenario) as p:
             return_value = cb.activate()
         assert return_value is None
-        
-        #Checking at the end of the list
+
+        # Checking at the end of the list
         def scenario():
             for i in range(num_elements):
-                cb.move_down() #KEY_DOWN x3
-            cb.deactivate() #KEY_LEFT
+                cb.move_down()  # KEY_DOWN x3
+            cb.deactivate()  # KEY_LEFT
             assert not cb.in_foreground
+
         with patch.object(cb, 'idle_loop', side_effect=scenario) as p:
             return_value = cb.activate()
         assert return_value is None
 
     def test_enter_on_last_returns_right(self):
         num_elements = 3
-        contents = [["A"+str(i), "a"+str(i)] for i in range(num_elements)]
+        contents = [["A" + str(i), "a" + str(i)] for i in range(num_elements)]
         cb = Checkbox(contents, get_mock_input(), get_mock_output(), name=cb_name)
         cb.refresh = lambda *args, **kwargs: None
 
-        #Checking at other elements - shouldn't return
+        # Checking at other elements - shouldn't return
         def scenario():
-            cb.select_entry() #KEY_ENTER
-            assert cb.in_foreground #Should still be active
-            cb.deactivate() #because is not deactivated yet and would idle loop otherwise
+            cb.select_entry()  # KEY_ENTER
+            assert cb.in_foreground  # Should still be active
+            cb.deactivate()  # because is not deactivated yet and would idle loop otherwise
+
         with patch.object(cb, 'idle_loop', side_effect=scenario) as p:
             return_value = cb.activate()
         assert return_value is None
-        
-        #Scrolling to the end of the list and pressing Enter - should return a correct dict
+
+        # Scrolling to the end of the list and pressing Enter - should return a correct dict
         def scenario():
             for i in range(num_elements):
-                cb.move_down() #KEY_DOWN x3
-            cb.select_entry() #KEY_ENTER
+                cb.move_down()  # KEY_DOWN x3
+            cb.select_entry()  # KEY_ENTER
             assert not cb.in_foreground
+
         with patch.object(cb, 'idle_loop', side_effect=scenario) as p:
             return_value = cb.activate()
         assert isinstance(return_value, dict)
--- ui/tests/test_config_manager.py
+++ ui/tests/test_config_manager.py
@@ -3,11 +3,11 @@ import logging
 import os
 import unittest
 
-os.sys.path.append(os.path.dirname(os.path.abspath('.')))
-
 from ui.config_manager import UIConfigManager
 
-#set up logging
+os.sys.path.append(os.path.dirname(os.path.abspath('.')))
+
+# set up logging
 LOG_FORMAT = '%(levelname)s %(asctime)-15s %(name)s  %(message)s'
 logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,14 +18,24 @@ class TestUIConfigManager(unittest.TestCase):
 
     def test_update_config(self):
         cm = UIConfigManager()
-        config_a = {"a":{"this1":"is original"}, "b":{"value":"to_be_replaced"}, 
-                       "c":"old_element_value", "e":{"this":"remains"}}
-        config_b = {"a":{"this2":"is_new"}, "b":{"value":"was_replaced"}, 
-                       "c":"new_element_value", "d":{"this":"is_new"}}
+        config_a = {
+            "a": {"this1": "is original"},
+            "b": {"value": "to_be_replaced"},
+            "c": "old_element_value",
+            "e": {"this": "remains"}
+        }
+        config_b = {
+            "a": {"this2": "is_new"},
+            "b": {"value": "was_replaced"},
+            "c": "new_element_value",
+            "d": {"this": "is_new"}
+        }
         new_config = cm.update_config(config_a, config_b)
-        self.assertEquals(new_config, {'a': {'this1': 'is original', 'this2': 'is_new'}, 
-                                  'c': 'new_element_value', 'b': {'value': 'was_replaced'},
-                                  'e': {'this': 'remains'}, 'd': {'this': 'is_new'}})
+        self.assertEquals(new_config, {
+            'a': {'this1': 'is original', 'this2': 'is_new'}, 'c': 'new_element_value',
+            'b': {'value': 'was_replaced'}, 'e': {'this': 'remains'},
+            'd': {'this': 'is_new'}
+        })
 
 
 if __name__ == '__main__':
