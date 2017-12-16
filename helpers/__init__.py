7e1b5ef7f00d197fbb4a1a4fae064819fada7f1a
CRImier <crimier@yandex.ru>
2017-12-16 22:20:40 +0200

    Adding an "install from pip" helper

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,3 +2,4 @@ from usability import ExitHelper
 from runners import BooleanEvent, Oneshot, BackgroundRunner
 from config_parse import read_config, write_config, read_or_create_config
 from general import local_path_gen
+from git_update import install_from_pip
