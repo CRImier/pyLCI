fa00c15968132d70abf320d4e443f1c0076562b6
monsieur_h <ray.hubert@riseup.net>
2017-12-16 23:39:34 +0100

    fixes variable naming

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 git pull
-$SUDO rsync -av --delete ./  $INSTALL_DIR
-$SUDO systemctl restart zpui.service
+${SUDO} rsync -av --delete ./  ${INSTALL_DIR}
+${SUDO} systemctl restart zpui.service
 
