96c34173de0a6863f31aab251b3ea8132a2edd11
CRImier <crimier@yandex.ru>
2017-12-19 01:32:32 +0200

    Making sure mocp commands don't print anything when called from nonmaskable callbacks

--- apps/media_apps/mocp/main.py
+++ apps/media_apps/mocp/main.py
@@ -8,20 +8,23 @@ i = None
 o = None
 
 #MOCP commands
-def mocp_command(*command):
+def mocp_command(*command, **options):
     try:
         return call(['mocp'] + list(command))
     except:
-        Printer(["Oops", "Is mocp there?"], i, o, 1)
+        #We shouldn't print anything to the screen if called from a non-maskable callback
+        silent = options.get("silent", False)
+        if not silent:
+            Printer(["Oops", "Is mocp there?"], i, o, 1)
 
 def mocp_toggle_play():
     mocp_command("-G")
 
-def mocp_next():
-    mocp_command("-f")
+def mocp_next(silent=False):
+    mocp_command("-f", silent=silent)
 
-def mocp_prev():
-    mocp_command("-r")
+def mocp_prev(silent=False):
+    mocp_command("-r", silent=silent)
 
 def option_switch_dialog(option):
     answer = DialogBox([["On", 'o'], ["Off", 'u'], ["Toggle", "t"]], i, o, message=option.capitalize()+":", name="MOCP {} option control dialog".format(option)).activate()
@@ -33,9 +36,6 @@ autonext_dialog = lambda: option_switch_dialog("autonext")
 
 def mocp_switch_option(switch_type, option):
     mocp_command("-{}".format(switch_type), option)
-
-def mocp_prev():
-    mocp_command("-r")
 
 main_menu_contents = [ 
 ["Toggle play/pause", mocp_toggle_play],
@@ -49,8 +49,8 @@ main_menu_contents = [
 def init_app(input, output):
     global main_menu, callback, i, o
     i = input; o = output
-    i.set_nonmaskable_callback("KEY_PROG1", mocp_next)
-    i.set_nonmaskable_callback("KEY_CAMERA", mocp_prev)
+    i.set_nonmaskable_callback("KEY_PROG1", lambda: mocp_next(silent=True) )
+    i.set_nonmaskable_callback("KEY_CAMERA", lambda: mocp_prev(silent=True) )
     main_menu = Menu(main_menu_contents, i, o, "MOCP menu")
     callback = main_menu.activate
 
