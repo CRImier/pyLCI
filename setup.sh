cfae71a5061c05675b1be5ddf26f1ec80ee6235b
monsieur_h <ray.hubert@riseup.net>
2017-12-18 10:59:50 +0100

    uses best practice syntax for bash variable usage

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+
 INSTALL_DIR="/opt/zpui"
 SUDO=''
 if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 [ -f config.json ] || cp config.json.example config.json
-$SUDO apt-get install python python-pip python-smbus python-dev python-pygame libjpeg-dev python-serial nmap
-$SUDO pip2 install -U luma.oled python-nmap smspdu zerophone_hw gpio vobject
-$SUDO mkdir -p $INSTALL_DIR
-$SUDO cp ./. $INSTALL_DIR -R
-cd $INSTALL_DIR
-$SUDO cp zpui.service /etc/systemd/system/
-$SUDO systemctl daemon-reload
-$SUDO systemctl enable zpui.service
-$SUDO systemctl start zpui.service
+${SUDO} apt-get install python python-pip python-smbus python-dev python-pygame libjpeg-dev python-serial nmap
+${SUDO} pip2 install -U luma.oled python-nmap smspdu zerophone_hw gpio vobject
+${SUDO} mkdir -p ${INSTALL_DIR}
+${SUDO} cp ./. ${INSTALL_DIR} -R
+cd ${INSTALL_DIR}
+${SUDO} cp zpui.service /etc/systemd/system/
+${SUDO} systemctl daemon-reload
+${SUDO} systemctl enable zpui.service
+${SUDO} systemctl start zpui.service
 
