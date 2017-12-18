6f7a9aa754439a2b229e91c39fc41cc2a6a65904
monsieur_h <ray.hubert@riseup.net>
2017-12-18 08:29:51 +0100

    makes update.sh run tests and dependencies installation in a temp dir. So any issue during the process doesn't break the current instance of ZPUI

--- update.sh
+++ update.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+TMP_DIR="/tmp/zpui_update"
 INSTALL_DIR="/opt/zpui"
-SUDO=''
+
 
 cd "$(dirname "$0")"  # Make sure we are in the script's directory when running
 set -e  # Strict mode : script stops if any command fails
 
-
+SUDO=''
 if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 
+# cleanup update dir
+if [ -d ${TMP_DIR} ]; then
+    rm -rf ${TMP_DIR}
+fi
+mkdir -p ${TMP_DIR}
+
+# update a copy of zpui in TMP_DIR
+git clone . ${TMP_DIR}
+cd ${TMP_DIR}
 git pull origin master  # Always tell the branch we pull from
 
 ${SUDO} pip install -r requirements.txt  # Make sure we have the latest dependencies installed
