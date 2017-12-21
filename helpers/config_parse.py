4ed4a40f09e94428d114136cfd318d1cb271b8d1
monsieur_h <ray.hubert@riseup.net>
2017-12-21 08:26:41 +0100

    changes behaviour (#38)

--- helpers/config_parse.py
+++ helpers/config_parse.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
 import json
 
+import os
+import shutil
+
 
 def read_config(config_path):
     with open(config_path, 'r') as f:
@@ -14,10 +17,15 @@ def write_config(config_dict, config_path):
 
 
 def read_or_create_config(config_path, default_config, app_name):
+    # type: (str, str, str) -> dict
+    # reads the config in `config_path` if invalid, replaces it with `default_config` and saves the erroneous config
+    # in `config_path`.failed
     try:
         config_dict = read_config(config_path)
     except (ValueError, IOError):
         print("{}: broken/nonexistent config, restoring with defaults...".format(app_name))
+        if os.path.exists(config_path):
+            shutil.move(config_path, config_path + ".failed")
         with open(config_path, 'w') as f:
             f.write(default_config)
         config_dict = read_config(config_path)
