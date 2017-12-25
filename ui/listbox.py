8292900e80f98f585170150e7f38082f7c3081cd
CRImier <crimier@yandex.ru>
2017-12-25 23:13:25 +0200

    Making the second element in Listbox optional

--- ui/listbox.py
+++ ui/listbox.py
@@ -12,7 +12,7 @@ class Listbox(BaseListUIElement):
 
       Listbox entry is a list, where:
          * ``entry[0]`` (entry's label) is usually a string which will be displayed in the UI, such as "Option 1". If ``entry_height`` > 1, can be a list of strings, each of those strings will be shown on a separate display row.
-         * ``entry[1]`` (entry's value) is the value to be returned when entry is selected.
+         * ``entry[1]`` (entry's value) is the value to be returned when entry is selected. If it's not supplied, entry's label is returned instead.
 
       *If you want to set contents after the initalisation, please, use set_contents() method.*
     * ``pointer``: currently selected entry's number in ``self.contents``.
@@ -44,7 +44,10 @@ class Listbox(BaseListUIElement):
     def get_return_value(self):
         if self.selected_entry is None:
             return None
-        return self.contents[self.selected_entry][1]
+        if len(self.contents[self.selected_entry]) == 1:
+            return self.contents[self.selected_entry][0]
+        else:
+            return self.contents[self.selected_entry][1]
 
     @to_be_foreground
     def select_entry(self):
