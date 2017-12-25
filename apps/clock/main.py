5184c8c48a6e64f91a9713a437055ed73be096e7
CRImier <crimier@yandex.ru>
2017-12-25 09:45:05 +0200

    Removing commented out code

--- apps/clock/main.py
+++ apps/clock/main.py
@@ -73,17 +73,3 @@ class ClockApp(ZeroApp, Refresher, CenteredTextRenderer):
     def on_start(self):
         super(ClockApp, self).on_start()
         self.refresher.activate()
-
-# def show_time():
-#     now = datetime.now()
-#     return [now.strftime("%H:%M:%S").center(o.cols), now.strftime("%Y-%m-%d").center(o.cols)]
-
-# i = None; o = None
-#
-# def callback():
-#     Refresher(show_time, i, o, 1, name="Clock refresher").activate()
-#
-# def init_app(input, output):
-#     global i, o
-#     i = input; o = output #Getting references to output and input device objects and saving them as globals
-#
