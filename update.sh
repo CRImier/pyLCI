9704bc743bf3e72cf1ce9dd27dfdac7ae5dbaf87
CRImier <crimier@yandex.ru>
2017-12-25 23:50:43 +0200

    Temporarily working around #59

--- update.sh
+++ update.sh
@@ -19,14 +19,14 @@ if (( $EUID != 0 )); then
 fi
 
 # cleanup update dir
-if [ -d ${TMP_DIR} ]; then
-    rm -rf ${TMP_DIR}
-fi
-mkdir -p ${TMP_DIR}
+#if [ -d ${TMP_DIR} ]; then
+#    rm -rf ${TMP_DIR}
+#fi
+#mkdir -p ${TMP_DIR}
 
 # update a copy of zpui in TMP_DIR
-git clone . ${TMP_DIR}
-cd ${TMP_DIR}
+#git clone . ${TMP_DIR}
+#cd ${TMP_DIR}
 
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
@@ -35,13 +35,15 @@ BASE=$(git merge-base @ "$UPSTREAM")
 
 
 # from https://stackoverflow.com/questions/3258243/check-if-pull-needed-in-git#3278427
-if [ ${LOCAL} = ${REMOTE} ]; then
-    echo "Already Up-to-date"
-    exit 126  # 1, 127 and 128 error codes are used by git, 126 is free
-fi
+#if [ ${LOCAL} = ${REMOTE} ]; then
+#    echo "Already Up-to-date"
+#    exit 126  # 1, 127 and 128 error codes are used by git, 126 is free
+#fi
 
 # --ff-only Refuse to merge and exit with a non-zero status unless the current HEAD is already up to date or the merge can be resolved as a fast-forward.
-git pull --ff-only origin master
+#git pull origin master
+#WORKAROUND only pulling the current branch
+git pull
 
 ${SUDO} pip install -r requirements.txt  # Make sure we have the latest dependencies installed
 
@@ -53,5 +55,5 @@ fi
 ${SUDO} mkdir -p ${INSTALL_DIR}
 ${SUDO} rsync -av --delete ./  ${INSTALL_DIR}
 ${SUDO} systemctl restart zpui.service
-rm -rf ${TMP_DIR}
+#rm -rf ${TMP_DIR}
 exit 0
