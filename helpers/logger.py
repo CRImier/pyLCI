d529dd59f323c24702af05c3de76b885859454e9
monsieur_h <ray.hubert@riseup.net>
2017-12-27 08:21:49 +0100

    falls back to loglevel.WARNING if the loglevel is invalid

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,9 +1,13 @@
 import logging
 
+
 def setup_logger(logger_name, log_level="warning"):
     # type: (str, str) -> logging.Logger
-    # todo : read level from config here
-    logger_level_attr = getattr(logging, log_level.upper())
+    try:
+        logger_level_attr = getattr(logging, log_level.upper())
+    except AttributeError:
+        logger_level_attr = logging.WARNING
+
     logger = logging.getLogger(logger_name)
     logger.setLevel(logger_level_attr)
     return logger
