c33b3f43516ac952cdd4f8cf7798a5bb467fc348
CRImier <crimier@yandex.ru>
2017-12-30 06:32:14 +0200

    Revert "falls back to loglevel.WARNING if the loglevel is invalid"
    This reverts commit d529dd59f323c24702af05c3de76b885859454e9.

--- helpers/logger.py
+++ helpers/logger.py
@@ -1,13 +1,9 @@
 import logging
 
-
 def setup_logger(logger_name, log_level="warning"):
     # type: (str, str) -> logging.Logger
-    try:
-        logger_level_attr = getattr(logging, log_level.upper())
-    except AttributeError:
-        logger_level_attr = logging.WARNING
-
+    # todo : read level from config here
+    logger_level_attr = getattr(logging, log_level.upper())
     logger = logging.getLogger(logger_name)
     logger.setLevel(logger_level_attr)
     return logger
