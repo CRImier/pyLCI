e1a63fb7f96b537a49f4cd52e6a223a3cba15de9
monsieur_h <ray.hubert@riseup.net>
2018-01-07 20:33:41 +0100

    #50 restart ofono-bridge development

--- ofono/bridge.py
+++ ofono/bridge.py
@@ -2,9 +2,6 @@ import logging
 
 import pydbus
 
-from apps.personal.contacts.address_book import AddressBook
-from apps.personal.contacts.vcard_converter import VCardContactConverter
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
@@ -16,16 +13,4 @@ class OfonoBridge(object):
 
     def __init__(self):
         super(OfonoBridge, self).__init__()
-        self.ofono_bus = pydbus.SystemBus().get('org.ofono', '/')  # todo : trycatch
-
-    def get_sim_contacts(self):
-        # type: () -> list
-        # returns the contacts list stored on the default sim-card
-        vcard_string = self.ofono_bus['Phonebook'].Import(timeout=100)  # todo : test this
-        return VCardContactConverter.from_string(vcard_string)
-
-    def import_sim_phonebook(self):
-        address_book = AddressBook()
-        for contact in self.get_sim_contacts():
-            address_book.add_contact(contact)
-        address_book.save_to_file()
+        self.ofono_bus = pydbus.SystemBus().get('org.ofono', '/')
