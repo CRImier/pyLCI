eae3fd5cbc6cf01765bf79c36df11b483afe1e19
Jon Maier <bolens1112@gmail.com>
2018-01-25 18:48:19 -0500

    Added extra keys for emulator these include:
    F1, F2, F5, F6,
    
    Key Pad: 0-9, +(AWNSER), -(HANGUP), *, /(HASH)
    
    Number row: =(+ which is ANSWER), -(HANGUP)

--- input/drivers/pygame_input.py
+++ input/drivers/pygame_input.py
@@ -6,27 +6,22 @@ import emulator
 from helpers import setup_logger
 from skeleton import InputSkeleton
 
-logger = setup_logger(__name__, "warning")
+logger = setup_logger(__name__, "debug")
 
-KP_KEYS = [
-    'KP0', 'KP1', 'KP2', 'KP3', 'KP4', 'KP5', 'KP6', 'KP7', 'KP8', 'KP9'
-]
 USED_KEYS = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'KP0', 'KP1', 'KP2', 'KP3', 'KP4', 'KP5', 'KP6', 'KP7', 'KP8', 'KP9',
-    'UP', 'DOWN', 'LEFT', 'RIGHT', 'RETURN', 'PAGEUP', 'PAGEDOWN'
+    'UP', 'DOWN', 'LEFT', 'RIGHT', 'RETURN', 'PAGEUP', 'PAGEDOWN',
+    'ASTERISK', 'HASH', 'PLUS', 'MINUS', 'EQUALS', 'F1', 'F2', 'F5', 'F6',
+    'KP_MULTIPLY', 'KP_DIVIDE', 'KP_PLUS', 'KP_MINUS',
+    'RSHIFT', 'LSHIFT'
 ]
-])
 
 KEY_MAP = dict([
     (getattr(pygame, 'K_' + key_name), key_name)
     for key_name in USED_KEYS
 ])
 
-KP_MAP = dict([
-    (getattr(pygame, 'K_' + key_name), key_name)
-    for key_name in KP_KEYS
-])
 
 
 class InputDevice(InputSkeleton):
@@ -43,7 +38,6 @@ class InputDevice(InputSkeleton):
         """
         Blocking event loop which just calls supplied callbacks in the keymap.
         """
-
         #TODO: debug and fix race condition
         while not hasattr(self, "emulator"):
             logger.debug("Input emulator not yet ready (a bug, TOFIX)")
@@ -55,17 +49,24 @@ class InputDevice(InputSkeleton):
                 continue
 
             key = event['key']
-
             if key not in KEY_MAP:
                 logger.debug('Ignoring key %s' % key)
                 continue
-
             key_name = 'KEY_' + KEY_MAP[key]
             if key_name == 'KEY_RETURN':
                 key_name = 'KEY_ENTER'
-
-            if key_name == 'KEY_' + KP_MAP[key]:
-                key_name = 'KEY_' + KP_MAP[key].replace('KP', '')
+            if key_name == 'KEY_KP_ENTER':
+                key_name = 'KEY_ENTER'
+            if key_name == 'KEY_KP_MULTIPLY':
+                key_name = 'KEY_ASTERISK'
+            if key_name == 'KEY_KP_DIVIDE':
+                key_name = 'KEY_HASH'
+            if key_name == 'KEY_MINUS' or key_name == 'KEY_KP_MINUS':
+                key_name = 'KEY_HANGUP'
+            if key_name == 'KEY_EQUALS' or key_name == 'KEY_KP_PLUS':
+                key_name = 'KEY_ANSWER'
+            if 'KP' in key_name:
+                key_name = 'KEY_' + KEY_MAP[key].replace('KP', '')
             logger.debug('Mapped key %s' % key_name)
 
             self.send_key(key_name)
