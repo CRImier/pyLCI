d7244c15c115d533f5e77fe3bb69e792d10e0bba
Jon Maier <bolens1112@gmail.com>
2018-01-25 19:41:33 -0500

    Removed if statements in favor of a dictionary lookup.

--- input/drivers/pygame_input.py
+++ input/drivers/pygame_input.py
@@ -6,15 +6,23 @@ import emulator
 from helpers import setup_logger
 from skeleton import InputSkeleton
 
-logger = setup_logger(__name__, "warning")
+logger = setup_logger(__name__, "debug")
 
+mapping = {'KEY_RETURN':'KEY_ENTER',
+           'KEY_KP_ENTER':'KEY_ENTER',
+           'KEY_KP_MULTIPLY':'KEY_ASTERISK',
+           'KEY_KP_DIVIDE':'KEY_HASH',
+           'KEY_MINUS':'KEY_HANGUP',
+           'KEY_KP_MINUS':'KEY_HANGUP',
+           'KEY_EQUALS':'KEY_ANSWER',
+           'KEY_KP_PLUS':'KEY_ANSWER'
+           }
 USED_KEYS = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'KP0', 'KP1', 'KP2', 'KP3', 'KP4', 'KP5', 'KP6', 'KP7', 'KP8', 'KP9',
     'UP', 'DOWN', 'LEFT', 'RIGHT', 'RETURN', 'PAGEUP', 'PAGEDOWN',
     'ASTERISK', 'HASH', 'PLUS', 'MINUS', 'EQUALS', 'F1', 'F2', 'F5', 'F6',
     'KP_MULTIPLY', 'KP_DIVIDE', 'KP_PLUS', 'KP_MINUS',
-    'RSHIFT', 'LSHIFT'
 ]
 
 KEY_MAP = dict([
@@ -53,18 +61,8 @@ class InputDevice(InputSkeleton):
                 logger.debug('Ignoring key %s' % key)
                 continue
             key_name = 'KEY_' + KEY_MAP[key]
-            if key_name == 'KEY_RETURN':
-                key_name = 'KEY_ENTER'
-            if key_name == 'KEY_KP_ENTER':
-                key_name = 'KEY_ENTER'
-            if key_name == 'KEY_KP_MULTIPLY':
-                key_name = 'KEY_ASTERISK'
-            if key_name == 'KEY_KP_DIVIDE':
-                key_name = 'KEY_HASH'
-            if key_name == 'KEY_MINUS' or key_name == 'KEY_KP_MINUS':
-                key_name = 'KEY_HANGUP'
-            if key_name == 'KEY_EQUALS' or key_name == 'KEY_KP_PLUS':
-                key_name = 'KEY_ANSWER'
+            key_name = mapping.get(key_name, key_name)
+
             if 'KP' in key_name:
                 key_name = 'KEY_' + KEY_MAP[key].replace('KP', '')
             logger.debug('Mapped key %s' % key_name)
