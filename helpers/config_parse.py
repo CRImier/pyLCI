db03cfccc8da259d71f65d11693ee5b1a3bcb1b8
CRImier <crimier@yandex.ru>
2018-06-01 09:25:57 +0300

    Adding a 'add keys from default config' function to read_or_create_config

--- helpers/config_parse.py
+++ helpers/config_parse.py
@@ -22,7 +22,8 @@ def read_or_create_config(config_path, default_config, app_name):
     # type: (str, str, str) -> dict
     """
     reads the config in `config_path` if invalid, replaces it with `default_config` and saves the erroneous config
-    in `config_path`.failed
+    in `config_path`.failed. Also, if the config is a dictionary, adds keys to it if
+    they're present in the default config but not in the current config.
 
     >>> print('{"configtype":"sample", "version":1}', file=open('/tmp/a_valid_config_file',"w"))
     >>> c = read_or_create_config("/tmp/a_valid_config_file", '{"default_config":True}', "test_runner")
@@ -56,6 +57,16 @@ def read_or_create_config(config_path, default_config, app_name):
         with open(config_path, 'w') as f:
             f.write(default_config)
         config_dict = read_config(config_path)
+    default_config_obj = json.loads(default_config)
+    keys_added = False
+    if isinstance(default_config_obj, dict):
+        for key, value in default_config_obj.items():
+            if key not in config_dict.keys():
+                config_dict[key] = value
+                keys_added = True
+                logger.debug("Adding key {} (from the default config) to the config for {}!".format(key, app_name))
+        if keys_added:
+            logger.warning("Added keys from default config to app {} - changes will not be preserved until the next time config is saved!".format(app_name))
     return config_dict
 
 def save_config_gen(path):
