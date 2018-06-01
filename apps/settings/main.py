8326c95d903f46c86d7e28180f0b85051e8fc8ca
CRImier <crimier@yandex.ru>
2018-06-01 16:46:05 +0300

    A new commandline - but a better solution needs to be found

--- apps/settings/main.py
+++ apps/settings/main.py
@@ -192,7 +192,7 @@ class GitUpdater(GenericUpdater):
         GitInterface.pull(branch = current_branch_name)
 
     def do_tests(self):
-        commandline = "python -B -m pytest --doctest-modules -v --doctest-ignore-import-errors --ignore=output/drivers --ignore=input/drivers --ignore=apps/hardware_apps/status/ --ignore=apps/test_hardware"
+        commandline = "python -B -m pytest --doctest-modules -v --doctest-ignore-import-errors --ignore=output/drivers --ignore=input/drivers --ignore=apps/hardware_apps/status/ --ignore=apps/test_hardware --ignore=apps/hardware_apps --ignore=docs/"
         output = check_output(commandline.split(" "))
         logger.debug("pytest output:")
         logger.debug(output)
