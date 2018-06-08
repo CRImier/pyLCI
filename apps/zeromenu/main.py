db9752acbb5da177b3169b4b321c1ee4a47fbb17
CRImier <crimier@yandex.ru>
2018-06-08 16:51:34 +0300

    Adding app open entry management to ZeroMenu

--- apps/zeromenu/main.py
+++ apps/zeromenu/main.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 from threading import Event
 
-from ui import Menu, PrettyPrinter, Checkbox
+from ui import Menu, PrettyPrinter, Checkbox, Listbox
 from helpers import read_or_create_config, local_path_gen, save_config_gen
 
 i = None
@@ -14,7 +14,7 @@ zeromenu = None
 
 screenshot_folder = "screenshots"
 
-default_config = '{"ordering":[], "excluded_actions":[]}'
+default_config = '{"ordering":[], "excluded_actions":[], "app_open_entries":[]}'
 config_filename = "config.json"
 
 local_path = local_path_gen(__name__)
@@ -40,6 +40,42 @@ def set_context(received_context):
 def set_ordering():
     pass
 
+def get_menu_name_for_alias(cl, alias):
+    menu_names = {app["name"]:app["menu_name"] for app in cl}
+    return menu_names.get(alias, None)
+
+def app_open_entries():
+    global config
+
+    def remove_entry_menu(): # Kinda misleading, since it's a Listbox
+        cl = context.list_contexts()
+        gmnfa = get_menu_name_for_alias
+        lc = [[gmnfa(cl, entry), entry] for entry in config["app_open_entries"] if gmnfa(cl, entry)]
+        if not lc:
+            PrettyPrinter("No app open entries found!", i, o, 3)
+            return
+        choice = Listbox(lc, i, o, name="Zeromenu app open entry removal menu").activate()
+        if choice:
+            config["app_open_entries"].remove(choice)
+            save_config(config)
+
+    def add_entry_menu():
+        cl = context.list_contexts()
+        app_context_list = [entry for entry in cl if entry["menu_name"]]
+        lc = [[e["menu_name"], e["name"]] for e in app_context_list]
+        lc = list(sorted(lc))
+        if not lc:
+            PrettyPrinter("No suitable app contexts found!", i, o, 3)
+            return
+        choice = Listbox(lc, i, o, name="Zeromenu app open entry addition menu").activate()
+        if choice:
+            config["app_open_entries"].append(choice)
+            save_config(config)
+
+    mc = [["Add an entry", add_entry_menu],
+          ["Remove an entry", remove_entry_menu]]
+    Menu(mc, i, o, name="Zeromenu app open entry settings menu").activate()
+
 def manage_contents():
     global config
     cc = []
@@ -60,7 +96,8 @@ def manage_contents():
         save_config(config)
 
 def settings_menu():
-    mc = [["Manage contents", manage_contents]]
+    mc = [["Manage contents", manage_contents],
+          ["App open entries", app_open_entries]]
     #      ["Set ordering", set_ordering]]
     Menu(mc, i, o, name="ZeroMenu settings menu").activate()
 
@@ -77,6 +114,12 @@ def init_app(input, output):
                 if action["aux_cb"]:
                     entry.append(action["aux_cb"])
                 mc.append(entry)
+        if config["app_open_entries"]:
+            context_list = [entry for entry in context.list_contexts()]
+            for context_alias in config["app_open_entries"]:
+                menu_name = get_menu_name_for_alias(context_list, context_alias)
+                if menu_name:
+                    mc.append([menu_name+" app", lambda x=context_alias: context.request_switch(x)])
         mc.append(["ZM settings", settings_menu])
         return mc
     zeromenu = Menu([], i, o, name="ZeroMenu main menu", contents_hook=get_contents)
