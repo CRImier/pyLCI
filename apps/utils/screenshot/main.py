4b0bad731586cc0ba45461caae95fcbf38b7e936
CRImier <crimier@yandex.ru>
2018-06-10 15:16:48 +0300

    Separating a screenshot app into a separate app, adding an ability to view screenshots

---
+++ apps/utils/screenshot/main.py
@@ -0,0 +1,48 @@
+menu_name = "Screenshots"
+
+import os
+from datetime import datetime
+
+from ui import Menu, PrettyPrinter, GraphicsPrinter
+
+i = None
+o = None
+context = None
+
+screenshot_folder = "screenshots"
+
+def take_screenshot():
+    image = context.get_previous_context_image()
+    if image != None:
+        timestamp = datetime.now().strftime("%y%m%d-%H%M%S")
+        filename = "screenshot_{}.png".format(timestamp)
+        path = os.path.join(screenshot_folder, filename)
+        image.save(path, "PNG")
+        PrettyPrinter("Screenshot saved to {}".format(path), i, o)
+
+def set_context(received_context):
+    global context
+    context = received_context
+    context.register_action("screenshot", take_screenshot, "Take screenshot", description="Takes a screenshot from previous app")
+
+def init_app(input, output):
+    global i, o
+    i = input; o = output
+
+def show_screenshot(path):
+    GraphicsPrinter(path, i, o, 5, invert=False)
+
+def list_screenshots():
+    mc = []
+    screenshots = [file for file in os.listdir(screenshot_folder) if file.endswith('.png')]
+    for filename in screenshots:
+        date_part = filename.split('_', 1)[-1].rsplit('.')[0]
+        path = os.path.join(screenshot_folder, filename)
+        mc.append([date_part, lambda x=path: show_screenshot(x)])
+    mc = list(sorted(mc))
+    Menu(mc, i, o, name="Screenshot list ").activate()
+
+def callback():
+    list_screenshots()
+    #mc = [["Screenshots", list_screenshots]]
+    #Menu(mc, i, o, name="Screenshot app main menu").activate()
--- apps/zeromenu/main.py
+++ apps/zeromenu/main.py
@@ -1,7 +1,6 @@
 menu_name = "ZeroMenu"
 
 import os
-from datetime import datetime
 
 from ui import Menu, PrettyPrinter, Checkbox, Listbox
 from helpers import read_or_create_config, local_path_gen, save_config_gen
@@ -11,8 +10,6 @@ o = None
 context = None
 zeromenu = None
 
-screenshot_folder = "screenshots"
-
 default_config = '{"ordering":[], "excluded_actions":[], "app_open_entries":[]}'
 config_filename = "config.json"
 
@@ -20,21 +17,11 @@ local_path = local_path_gen(__name__)
 config = read_or_create_config(local_path(config_filename), default_config, menu_name+" app")
 save_config = save_config_gen(local_path(config_filename))
 
-def take_screenshot():
-    image = context.get_previous_context_image()
-    if image != None:
-        timestamp = datetime.now().strftime("%y%m%d-%H%M%S")
-        filename = "screenshot_{}.png".format(timestamp)
-        path = os.path.join(screenshot_folder, filename)
-        image.save(path, "PNG")
-        PrettyPrinter("Screenshot saved to {}".format(path), i, o)
-
 def set_context(received_context):
     global context
     context = received_context
     context.request_global_keymap({"KEY_PROG2":context.request_switch})
     context.set_target(zeromenu.activate)
-    context.register_action("screenshot", take_screenshot, "Take screenshot", description="Takes a screenshot from previous app")
 
 def set_ordering():
     pass
