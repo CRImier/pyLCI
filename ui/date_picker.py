e8fa8ffef2caaee89877b6094c1531d05405a852
derivmug <404-response@tuta.io>
2018-10-08 21:40:42 +0100

    Cleaner navigation code - no linejumps

--- ui/date_picker.py
+++ ui/date_picker.py
@@ -21,10 +21,15 @@ class DatePicker(BaseUIElement):
 		# Top-left cell is (1, 1)
 		self.selected_option = {'x': 1, 'y': 1}
 
-		self.year_month = "2018 - Oct"
+		self.current_month = 2
+		self.current_year = 2018
+		self.starting_weekday = 1
+
 		self.calendar_grid = []
 
 		self.cal = calendar.Calendar()
+
+		self._set_month_year(2, 2018)
 	
 	def generate_keymap(self):
 		return {
@@ -32,63 +37,31 @@ class DatePicker(BaseUIElement):
 			"KEY_LEFT": "move_left",
 			"KEY_UP": "move_up",
 			"KEY_DOWN": "move_down",
-			"KEY_ENTER": "accept_value"
+			"KEY_ENTER": "accept_value",
+			"KEY_PAGEUP": "next_month",
+			"KEY_PAGEDOWN": "previous_month"
 		}
 
 	def idle_loop(self):
 		sleep(0.1)
 
 	def move_right(self):
-		if self.selected_option['x'] < self.GRID_WIDTH:
-			if self._check_movable_field(self.selected_option['x']+1, self.selected_option['y']):
-				self.selected_option['x'] += 1
-				self.refresh()
-
-		elif self.selected_option['x'] == self.GRID_WIDTH and self.selected_option['y'] < self.GRID_HEIGHT:
-			if self._check_movable_field(1, self.selected_option['y']+1):
-				self.selected_option['x'] = 1
-				self.selected_option['y'] += 1
-				self.refresh()
+		self._move_cursor(1, 0)
 
 	def move_left(self):
-		if self.selected_option['x'] > 1:
-			if self._check_movable_field(self.selected_option['x']-1, self.selected_option['y']):
-				self.selected_option['x'] -= 1
-				self.refresh()
-
-		elif self.selected_option['x'] == 1 and self.selected_option['y'] > 1:
-			if self._check_movable_field(1, self.selected_option['y']-1):
-				self.selected_option['x'] = self.GRID_WIDTH
-				self.selected_option['y'] -= 1
-				self.refresh()
+		self._move_cursor(-1, 0)
 
 	def move_up(self):
-		if self.selected_option['y'] > 1:
-			if self._check_movable_field(self.selected_option['x'], self.selected_option['y']-1):
-				self.selected_option['y'] -= 1
-				self.refresh()
-
-		elif self.selected_option['y'] == 1:
-			if self._check_movable_field(self.selected_option['x'], self.GRID_HEIGHT):
-				self.selected_option['y'] = self.GRID_HEIGHT
-				self.refresh()
-			else:
-				self.selected_option['y'] = self.GRID_HEIGHT-1
-				self.refresh()
+		self._move_cursor(0, -1)
 
 	def move_down(self):
-		if self.selected_option['y'] < self.GRID_HEIGHT:
-			if self._check_movable_field(self.selected_option['x'], self.selected_option['y']+1):
-				self.selected_option['y'] += 1
-				self.refresh()
-			else:
-				self.selected_option['y'] = 1
-				self.refresh()
+		self._move_cursor(0, 1)
 
-		elif self.selected_option['y'] == self.GRID_HEIGHT:
-			if self._check_movable_field(self.selected_option['x'], 1):
-				self.selected_option['y'] = 1
-				self.refresh()
+	def next_month(self):
+		pass
+
+	def previous_month(self):
+		pass
 
 	def accept_value(self):
 		pass
@@ -96,10 +69,12 @@ class DatePicker(BaseUIElement):
 	def draw_calendar(self):
 		self.c.clear()
 
+		year_month = "{} - {}".format(self.MONTH_NAMES[self.current_month-1], self.current_year)
+
 		# Draw 'year - month' at the top
-		month_year_text_bounds = self.c.get_text_bounds(self.year_month)
-		centered_cords = self.c.get_centered_text_bounds(self.year_month)
-		self.c.text(self.year_month, (centered_cords[0], 0))
+		month_year_text_bounds = self.c.get_text_bounds(year_month)
+		centered_cords = self.c.get_centered_text_bounds(year_month)
+		self.c.text(year_month, (centered_cords[0], 0))
 
 		# Draw calendar grid
 		step_width = self.c.width / self.GRID_WIDTH
@@ -111,19 +86,10 @@ class DatePicker(BaseUIElement):
 			for y in range(step_height, self.c.height, step_height):
 				self.c.line((0, y, self.c.width, y))
 
-		# Draw dates
-		first_day, _ = calendar.monthrange(2018, 10)
-
-		# Assign -1 to empty cells
-		for i in range(first_day):
-			self.calendar_grid.append(-1)
-
-		i = first_day
-		for date in self.cal.itermonthdays(2018, 10):
-			if date == 0:
+		i = self.starting_weekday
+		for date in self.calendar_grid:
+			if date == -1:
 				continue
-
-			self.calendar_grid.append(date)
 
 			date_text_bounds = self.c.get_text_bounds(str(date))
 
@@ -133,10 +99,6 @@ class DatePicker(BaseUIElement):
 			self.c.text(str(date), (x_cord+1, y_cord+1))
 
 			i += 1
-
-		# Assign -1 to empty cells
-		for i in range(self.GRID_WIDTH*self.GRID_HEIGHT-i):
-			self.calendar_grid.append(-1)
 
 		# Highlight selected option
 		selected_x = (self.selected_option['x']-1)*step_width
@@ -148,10 +110,55 @@ class DatePicker(BaseUIElement):
 	def refresh(self):
 		self.draw_calendar()
 
+	# Moves the cursor
+	def _move_cursor(self, delta_x, delta_y):
+		if self._check_movable_field(self.selected_option['x']+delta_x, self.selected_option['y']+delta_y):
+			self.selected_option['x'] = self.selected_option['x'] + delta_x
+			self.selected_option['y'] += delta_y
+			self.refresh()
+
 	# Check for empty cells
-	def _check_movable_field(self, x, y):
-		if self.calendar_grid[(x-1)+(y-1)*self.GRID_WIDTH] != -1:
+	def _check_movable_field(self, new_x, new_y):
+		if (new_x-1)+(new_y-1)*self.GRID_WIDTH >= len(self.calendar_grid):
+			return False
+
+		if self.calendar_grid[(new_x-1)+(new_y-1)*self.GRID_WIDTH] != -1 and \
+			new_x <= self.GRID_WIDTH and \
+			new_x >= 1 and \
+			new_y <= self.GRID_HEIGHT and \
+			new_y >= 1:
+
 			return True
 		else:
 			return False
+
+	def _set_month_year(self, month, year):
+		self.current_month = month
+		self.current_year = year
+
+		# Get the weekday of the first day of the month
+		first_day, _ = calendar.monthrange(self.current_year, self.current_month)
+		self.starting_weekday = first_day
+
+		# Assign -1 to empty cells
+		for i in range(first_day):
+			self.calendar_grid.append(-1)
+
+		# Set the cursor to the first viable cell
+		self.selected_option['x'] = i+2
+
+		i = first_day
+		for date in self.cal.itermonthdays(self.current_year, self.current_month):
+			if date == 0:
+				continue
+
+			self.calendar_grid.append(date)
+
+			i += 1
+
+		# Assign -1 to empty cells
+		for i in range(self.GRID_WIDTH*self.GRID_HEIGHT-i):
+			self.calendar_grid.append(-1)
+
+
 
