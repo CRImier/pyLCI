debc9cded6680e43bf7154c60c0fc1a637c07360
Arsenijs <crimier@yandex.ru>
2018-11-14 19:18:26 +0000

    Update splash.py
    Co-Authored-By: LouisPi <granbobbi22@gmail.com>

--- splash.py
+++ splash.py
@@ -5,7 +5,7 @@ import PIL
 def splash(i, o):
     if o.size == (128, 64):
 	image = PIL.Image.open("splash.png").convert('L')
-    elif o.size == (128, 128):
+    elif (o.width, o.height) == (128, 128):
 	image = PIL.Image.open("splash_128x128.png").convert('L')
     else:
 	o.display_data("Welcome to", "ZPUI")
