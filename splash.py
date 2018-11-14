894f941e162d66dcb2b2098e6c46267523fd0dab
Louis Pi <granbobbi22@gmail.com>
2018-11-14 20:02:15 +0100

    splash.py changes

--- splash.py
+++ splash.py
@@ -3,8 +3,12 @@ from PIL import ImageOps
 import PIL
 
 def splash(i, o):
-    image = PIL.Image.open("splash.png").convert('L')
-    # image = PIL.Image.open("splash_128x128.png").convert('L')
+    if o.size == (128, 64):
+	image = PIL.Image.open("splash.png").convert('L')
+    elif o.size == (128, 128):
+	image = PIL.Image.open("splash_128x128.png").convert('L')
+    else:
+	o.display_data("ZPUI")
     image = ImageOps.invert(image)
     image = image.convert(o.device_mode)
     o.display_image(image)
