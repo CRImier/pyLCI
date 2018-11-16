b2a549cc4a4d9cd62a8087893c43281e8ee99ab2
Louis Pi <granbobbi22@gmail.com>
2018-11-16 20:44:14 +0100

    bug fixes and improved splash.py. TEST ON OTHER SCREENS FIRST PLEASE

--- output/drivers/st7735.py
+++ output/drivers/st7735.py
@@ -12,12 +12,12 @@ class Screen(LumaScreen, OutputDevice):
   default_h_offset = 0
   default_v_offset = 0
 
-  def init_display(self, **kwargs)
+  def init_display(self, **kwargs):
       self.rotate = kwargs.pop("rotate", self.default_rotate)
       self.h_offset = kwargs.pop("h_offset", self.default_h_offset)
       self.v_offset = kwargs.pop("v_offset", self.default_v_offset)
-      width = self.height if rotate%2==1 else self.width
-      height = self.width if rotate%2==1 else self.height
+      width = self.height if self.rotate%2==1 else self.width
+      height = self.width if self.rotate%2==1 else self.height
       self.device = st7735(self.serial, width=width, height=height, bgr=True, rotate=self.rotate, h_offset=self.h_offset, v_offset=self.v_offset)
 
   def enable_backlight(self, *args, **kwargs):
