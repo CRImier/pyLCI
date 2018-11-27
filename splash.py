25899b084e2f15b5d5010c153a2afa6394343fab
Louis Pi <granbobbi22@gmail.com>
2018-11-27 18:38:38 +0100

    fixed return

--- splash.py
+++ splash.py
@@ -22,7 +22,7 @@ def splash(i, o):
         image = ImageOps.expand(image, border=(left, top, right, bottom), fill="black")
     else:
 	o.display_data("Welcome to", "ZPUI")
-	return image
+	return 
     image = image.convert(o.device_mode)
     o.display_image(image)
 
