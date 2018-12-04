7c7fef795a72ef21e8036dfed0dea7ac1b7f31ad
CRImier <crimier@yandex.ru>
2018-12-04 18:25:24 +0200

    Broken key regen procedure, fixing

--- apps/network_apps/ssh/main.py
+++ apps/network_apps/ssh/main.py
@@ -30,17 +30,19 @@ def init_app(input_obj, output_obj):
     o = output_obj
 
 def regenerate_ssh_keys():
-    logger.info("Regenerating SSH keys")
-    ssh_dir = config["ssh_key_dir"]
-    key_files = [os.path.join(ssh_dir, f) for f in os.listdir(ssh_dir) if os.path.isfile(os.path.join(ssh_dir, f)) \
-                                                and f.startswith("ssh_host") and "key" in f]
-    for f in key_files:
-        logger.warning("Removing {}".format(f))
-        os.remove(f)
     try:
+        with LoadingIndicator(i, o, message="Scanning ports"):
+            logger.info("Regenerating SSH keys")
+            ssh_dir = config["ssh_key_dir"]
+            key_files = [os.path.join(ssh_dir, f) for f in os.listdir(ssh_dir) \
+                           if os.path.isfile(os.path.join(ssh_dir, f)) \
+                           and f.startswith("ssh_host") and "key" in f]
+        for f in key_files:
+            logger.warning("Removing {}".format(f))
+            os.remove(f)
         for command in config["key_regen_commands"]:
-            subprocess.call(command, shell)
-    except subprocess.CalledProcessError:
+            subprocess.call(command, shell=True)
+    except:
         logger.exception("Failed to regenerate keys!")
         Printer("Failed to regenerate keys!", i, o)
     else:
