fb470810fa4fee114102fd27b0940e7264cb0421
Louis Pi <granbobbi22@gmail.com>
2018-12-07 17:50:28 +0100

    small changes to resize method

--- ui/printer.py
+++ ui/printer.py
@@ -128,13 +128,13 @@ def GraphicsPrinter(image_or_path, i, o, sleep_time=1, invert=True):
 	    bigger_image_side = image_width
 	    smaller_image_side = image_height
      	bigger_side_percent = (bigger_side/float(bigger_image_side))
-    	other_size = int((float(smaller_image_side)*float(bigger_side_percent)))
-    	image = image.resize((bigger_side,other_size), Image.LANCZOS) # Resizes the image to the calculated dimensions to fit the screen using a high quality downsampling filter
-    elif (o.width, o.height) == image.size:
+    	other_size = int((float(smaller_image_side)*float(bigger_side_percent))) # Working out smaller side length
+    	image = image.resize((bigger_side,other_size), Image.ANTIALIAS) # Resizes the image to the calculated dimensions to fit the screen and stick to the aspect ratio using a high quality downsampling filter
+    elif (o.width, o.height) == image.size: # Checks if screen dimensions and exactly the same as image dimensions
 	pass
-    else:
+    else: # This should happen if the screen is smaller on one or both sides than the image
         size = o.width, o.height
-        image.thumbnail(size, Image.ANTIALIAS)
+        image.thumbnail(size, Image.ANTIALIAS) # Resizes the image sticking to the aspect ratio using
     if invert:
 	image = ImageOps.invert(image.convert('L'))
     left = top = right = bottom = 0
