bfa744eeb9ede3857b080bbd9bbe96fb7593f16b
CRImier <crimier@yandex.ru>
2018-12-10 20:49:03 +0200

    SIM800 modem flasher ready, now needs to be tested

--- apps/hardware_apps/hardware_setup/main.py
+++ apps/hardware_apps/hardware_setup/main.py
@@ -1,11 +1,17 @@
 menu_name = "Hardware setup"
 
 import os
+import sys
+import traceback
+from time import sleep
 
-from ui import Menu, PrettyPrinter as Printer, DialogBox, LoadingIndicator, PathPicker, Listbox #, UniversalInput, Refresher, IntegerAdjustInput
+from ui import Menu, PrettyPrinter as Printer, DialogBox, LoadingIndicator, PathPicker, Listbox, ProgressBar
 from helpers import setup_logger, read_or_create_config, local_path_gen, write_config, save_config_gen
 
 import smbus
+import gpio
+sys.excepthook = sys.__excepthook__
+gpio.log.setLevel(gpio.logging.INFO)
 
 from mtkdownload import MTKDownloadProcess, collect_fw_folders
 
@@ -21,7 +27,7 @@ o = None
 
 def change_settings():
     menu_contents = [
-    ["Set mtkdownload path", set_mtkdownload_path]]
+      ["Set mtkdownload path", set_mtkdownload_path]]
     Menu(menu_contents, i, o, "ZP hardware setup settings menu").activate()
 
 def set_mtkdownload_path():
@@ -30,6 +36,22 @@ def set_mtkdownload_path():
     if path:
         config["mtkdownload_path"] = path
         save_config(config)
+        return True
+    return False
+
+def get_gsm_reset_gpio():
+    hw_revs = [["Gamma", "gamma"], ["Delta", "delta"], ["Delta-B", "deltab"]]
+    gpios = {"gamma":502, "delta":496, "deltab": 496}
+    assert(all([hwr[1] in gpios.keys() for hwr in hw_revs])) # check after editing
+    hwr = Listbox(hw_revs, i, o, name="Hardware setup app GSM FW picker").activate()
+    if not hwr:
+        return None
+    if hwr in ["delta", "deltab"]:
+        # Enable UARTs
+        gpio.setup(500, gpio.OUT)
+        gpio.set(500, False)
+    gp = gpios[hwr]
+    return gp
 
 def flash_image_ui():
     if not MTKDownloadProcess(None, None, path=config["mtkdownload_path"]).mtkdownload_is_available():
@@ -37,14 +59,80 @@ def flash_image_ui():
         choice = DialogBox("yn", i, o, name="Hardware setup app mtkdownload path confirmation", message="Set mtkdownload path?").activate()
         if choice:
             if set_mtkdownload_path():
-                callback()
-            else:
-                return
+                # Running again, now the path should be valid
+                flash_image_ui()
+            return # No need to continue whether we've recursed or not, exiting
     files = collect_fw_folders(config["gsm_fw_path"])
     lbc = [[os.path.basename(file), file] for file in files]
     choice = Listbox(lbc, i, o, name="Hardware setup app GSM FW picker").activate()
+    # A ProgressBar for the flashing specifically
+    pb = ProgressBar(i, o, message="Flashing the modem")
+    # A LoadingIndicator for everything else
+    li = LoadingIndicator(i, o, message="Waiting for modem")
     if choice:
-        print(choice)
+        cb = lambda state: process_state(pb, li, state)
+        p = MTKDownloadProcess("/dev/ttyAMA0", choice, callback=cb, path=config["mtkdownload_path"])
+        gp = get_gsm_reset_gpio()
+        if not gp:
+            return
+        def reset():
+            gpio.setup(gp, gpio.OUT)
+            gpio.set(gp, False)
+            sleep(0.1)
+            gpio.set(gp, True)
+        e = None
+        try:
+            p.write_image(reset_cb=reset)
+        except Exception as e:
+            e = traceback.format_exc()
+        state = p.get_state()
+        if state["state"] == "failed" or e:
+            choice = DialogBox("yn", i, o, message="Send bugreport?").activate()
+            if choice:
+                br = BugReport("mtkdownload_flash_fail.zip")
+                if e:
+                    br.add_text(json.dumps(e), "mtkdownload_exception.json")
+                br.add_text(json.dumps(p.dump_info()), "mtkdownload_psinfo.json")
+                result = br.send_or_store("/boot/", logger=logger)
+                if result[0]:
+                    logger.info("Report sent to {}".format(result[1]))
+                else:
+                    logger.info("Report stored in {}".format(result[1]))
+
+last_state = None
+
+def process_state(pb, li, state):
+    # filtering repeated status to avoid wasting CPU power
+    global last_state
+    if state == last_state:
+        return
+    last_state = state
+    # actually processing the status
+    cs = state["state"]
+    if cs == "flashing":
+        progress = state["progress"]
+        li.pause()
+        pb.background_if_inactive()
+        if pb.progress != progress:
+            pb.progress = progress
+    elif cs in ["started", "processing", "not_started"]:
+        li.background_if_inactive()
+        pb.pause()
+        if cs == "started":
+            li.message = "Waiting for modem"
+        elif cs == "processing":
+            li.message = "Erasing flash"
+        elif cs == "not_started":
+            li.message = "Not started (?)"
+    elif cs in ["failed", "finished"]:
+        pb.stop()
+        li.stop()
+        if cs == "failed":
+            Printer("Failed to flash!", i, o, 5)
+        else:
+            Printer("Finished!", i, o, 5)
+    else:
+        logger.warning("Unrecognized status: {} (full: {})".format(cs, state))
 
 def init_app(input, output):
     global main_menu, callback, i, o
@@ -52,6 +140,6 @@ def init_app(input, output):
 
 def callback():
     main_menu_contents = [
-    ["Flash GSM modem", flash_image_ui],
-    ["Settings", change_settings]]
+      ["Flash GSM modem", flash_image_ui],
+      ["Settings", change_settings]]
     Menu(main_menu_contents, i, o, "ZP hardware setup menu").activate()
--- apps/hardware_apps/hardware_setup/mtkdownload.py
+++ apps/hardware_apps/hardware_setup/mtkdownload.py
@@ -1,7 +1,8 @@
 import os
+import sys
 from helpers import ProHelper, setup_logger
 
-logger = setup_logger(__name__, "debug")
+logger = setup_logger(__name__, "info")
 
 class MTKDownloadProcess(object):
     state = "not_started"
@@ -27,15 +28,17 @@ class MTKDownloadProcess(object):
     def gen_commandline(self, format_fat="Y"):
         return [self.mtkdownload_path, self.port, "ROM_VIVA", format_fat]
 
-    def write_image(self):
+    def write_image(self, reset_cb=None):
         self.state = "not_started"
         self.output = ""
         commandline = self.gen_commandline()
         p = ProHelper(commandline, cwd=self.fw_dir, use_terminal=True, output_callback=self.process_output)
+        if callable(reset_cb):
+            reset_cb()
         p.run()
         self.state = "started"
         while p.is_ongoing():
-            p.poll()
+            p.poll(read_type="readall_or_until", read_kw={"until":"\r"})
             if not p.is_ongoing():
                 rc = p.get_return_code()
                 if rc != 0:
@@ -43,21 +46,48 @@ class MTKDownloadProcess(object):
                 else:
                     self.state = "finished"
             if callable(self.callback):
-                self.callback(self.get_state)
+                self.callback(self.get_state())
 
     def process_output(self, output):
-        print(repr(output))
+        sys.stdout.flush()
         self.output += output
+        if self.is_rom_progress_output(output):
+            self.process_rom_progress_output(output)
+        elif self.is_detected_chip_output(output):
+            self.process_detected_chip_output(output)
+        else:
+            logger.info("Yet unrecognized input: {}".format( repr(output) ))
+
+    def is_detected_chip_output(self, output):
+        # '\t52' repeated a lot of times
+        return '\t52'*10 in output
+
+    def process_detected_chip_output(self, output):
+        self.state = "processing"
+
+    def is_rom_progress_output(self, output):
+        # example: 'Rom 87%\t 1672832 bytes sent   \r'
+        return output.startswith("Rom") and output.endswith('\r')
+
+    def process_rom_progress_output(self, output):
+        # example: 'Rom 87%\t 1672832 bytes sent   \r'
+        self.state = "flashing"
+        try:
+            progress = output.split(' ', 1)[1].split('\t', 1)[0]
+            self.progress = int(progress.strip('%'))
+        except:
+            logger.exception("can't parse progress string: {}".format(repr(output)))
 
     def get_state(self):
         s_d = {"state":self.state, "output":self.output}
-        if self.state in ["started", "finished", "not_started"]:
-            return s_d
-        elif self.state == "failed":
+        if self.state == "failed":
             s_d["returncode"] = self.p.get_return_code()
-            return s_d
+        elif self.state == "flashing":
+            s_d["progress"] = self.progress
+        return s_d
 
 def collect_fw_folders(base_path, key="SIM800", check_cfg=True, requirements=["ROM", "ROM_VIVA", "VIVA"]):
+    logger.info("Collecting \"{}\" firmware from {}".format(key, base_path))
     unsorted_paths = []
     for path in os.listdir(base_path):
         full_path = os.path.join(base_path, path)
@@ -71,12 +101,12 @@ def collect_fw_folders(base_path, key="SIM800", check_cfg=True, requirements=["R
                 with open(os.path.join(full_path, "sim_fw_zp_version")) as f:
                     version = f.read().strip()
             if check_cfg and not "{}.cfg".format(path) in fw_files:
-                logger.debug("{} doesn't have CFG, continuing".format(path))
+                logger.info("{} doesn't have CFG, continuing".format(path))
                 continue
             if not all([req in fw_files for req in requirements]):
-                logger.debug("{} doesn't have one of the requirements, continuing".format(path))
+                logger.info("{} doesn't have one of the requirements, continuing".format(path))
                 continue
             unsorted_paths.append([version, full_path])
-    logger.debug(unsorted_paths)
+    logger.info(unsorted_paths)
     paths = [path for v, path in reversed(sorted(unsorted_paths))]
     return paths
