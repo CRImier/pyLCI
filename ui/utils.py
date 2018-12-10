188fb78d58fef1db4b908d7a682c1e9aeaa7ce88
Louis Pi <granbobbi22@gmail.com>
2018-12-10 19:27:20 +0100

    more logging added

--- ui/utils.py
+++ ui/utils.py
@@ -221,6 +221,7 @@ def fit_image_to_screen(image, o):
 
     image_width, image_height = image.size
     if o.height > image_height and o.width > image.width: # Checks if the screen dimensions are equal to the image size
+	logger.info("Using the resize script")
         if o.height/image_height < o.width/image_width: # Checks which side is bigger in proportion to the image size
             bigger_side = o.height
             bigger_image_side = image_height
@@ -232,14 +233,16 @@ def fit_image_to_screen(image, o):
         bigger_side_percent = (bigger_side/float(bigger_image_side))
         other_size = int((float(smaller_image_side)*float(bigger_side_percent))) # Working out smaller side length
         image = image.resize((bigger_side,other_size), Image.ANTIALIAS) # Resizes the image to the calculated dimensions to fit the screen and stick to the aspect ratio using a $
-    elif (o.width, o.height) == image.size: # Checks if screen dimensions and exactly the same as image dimensions.
-        pass
+    elif (o.width, o.height) == image.size: # Checks if screen dimensions and exactly the same as image dimensions
+	logger.info("Screen size is already the same as the image size")
     elif (o.width == image_width and o.height < image_height) or (o.height == image_height and o.height < image_height):
-        pass
+        logger.info("One of the sides of the screen is the same as the corresponding image side and the other side is smaller than it's corresponding image side")
     else: # This should happen if the screen is smaller on one or both sides than the image
+	logger.info("Using the thumbnail script")
         size = o.width, o.height
         image.thumbnail(size, Image.ANTIALIAS) # Resizes the image sticking to the aspect ratio using
     if (o.width, o.height) != image.size:
+	logger.info("Borders being added")
         left = top = right = bottom = 0
         width, height = image.size
         if o.width > width:
