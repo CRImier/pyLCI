3e704ddbbd9eda1e79a756981793010d722ca98d
Louis Pi <granbobbi22@gmail.com>
2018-12-21 21:18:09 +0100

    added basic logging

--- ui/utils.py
+++ ui/utils.py
@@ -221,11 +221,14 @@ def fit_image_to_screen(image, o):
 
     image_width, image_height = image.size
     if o.height > image_height and o.width > image.width: # Checks if the screen dimensions are equal to the image size
+	logger.info("Using resize script")
         if o.height/image_height < o.width/image_width: # Checks which side is bigger in proportion to the image size
+	    logger.info("Using height as multiplier")
             bigger_side = o.height
             bigger_image_side = image_height
             smaller_image_side = image_width
         else:
+	    logger.info("Using width as multiplier")
             bigger_side = o.width
             bigger_image_side = image_width
             smaller_image_side = image_height
@@ -233,22 +236,27 @@ def fit_image_to_screen(image, o):
         other_size = int((float(smaller_image_side)*float(bigger_side_percent))) # Working out smaller side length
         image = image.resize((bigger_side,other_size), Image.ANTIALIAS) # Resizes the image to the calculated dimensions to fit the screen and stick to the aspect ratio using a $
     elif (o.width, o.height) == image.size: # Checks if screen dimensions and exactly the same as image dimensions
-	pass
+	logger.info("Exect same size - no changes needed")
     elif (o.width == image_width and o.height > image_height) or (o.height == image_height and o.width > image_width):
-        pass
+        logger.info("One side is the same, the other is bigger - borders needed")
     else: # This should happen if the screen is smaller on one or both sides than the image
+        logger.info("Using thumbnail script")
         size = o.width, o.height
         image.thumbnail(size, Image.ANTIALIAS) # Resizes the image sticking to the aspect ratio using
     if (o.width, o.height) != image.size:
+        logger.info("Adding borders")
         left = top = right = bottom = 0
         width, height = image.size
         if o.width > width:
+	    logger.info("Adding borders for width")
             delta = o.width - width
             left = delta // 2
             right = delta - left
         if o.height > height:
+	    logger.info("Adding borders for height")
             delta = o.height - height
             top = delta // 2
             bottom = delta - top
         image = ImageOps.expand(image, border=(left, top, right, bottom), fill="black")
+    logger.info("All resizing finished")
     return image
