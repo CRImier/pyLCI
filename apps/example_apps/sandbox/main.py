70b98af0896fa560bab2dd746b9d062a714e098d
CRImier <crimier@yandex.ru>
2018-12-23 04:31:57 +0200

    Improving the sandbox app a little (now the emulator window appears on load)

--- apps/example_apps/sandbox/main.py
+++ apps/example_apps/sandbox/main.py
@@ -1,12 +1,16 @@
 menu_name = "Python sandbox"
 
+from ui import Canvas
+
 i = None
 o = None
+c = None
 context = None
 
 def init_app(input, output):
-    global i, o
+    global i, o, c
     i = input; o = output
+    c = Canvas(o, interactive=True)
 
 def set_context(new_context):
     global context
@@ -14,4 +18,15 @@ def set_context(new_context):
 
 def callback():
     import code as __code__
-    __code__.interact(local=dict(globals(), **locals()))
+    c.display()
+    # Making sure that Ctrl+C is caught and doesn't cause the driver to crash...
+    # Well, doesn't really work
+    exit = False
+    while not exit:
+        try:
+            __code__.interact(local=dict(globals(), **locals()))
+        except KeyboardInterrupt:
+            pass
+        else:
+             exit = True
+             raise
