7d21bc127e5cf2b3130d67c35ebfe7d5bab42d28
Louis Pi <granbobbi22@gmail.com>
2019-01-02 13:37:31 +0100

    fixed typo

--- ui/utils.py
+++ ui/utils.py
@@ -243,7 +243,7 @@ def fit_image_to_screen(image, o):
         other_size = int((float(smaller_image_side)*float(bigger_side_percent))) # Working out smaller side length
         image = image.resize((bigger_side,other_size), Image.ANTIALIAS) # Resizes the image to the calculated dimensions to fit the screen and stick to the aspect ratio using a $
     elif (o.width, o.height) == image.size: # Checks if screen dimensions and exactly the same as image dimensions
-	logger.info("Exect same size - no changes needed")
+	logger.info("Exact same size - no changes needed")
     elif (o.width == image_width and o.height > image_height) or (o.height == image_height and o.width > image_width):
         logger.info("One side is the same, the other is bigger - borders needed")
     else: # This should happen if the screen is smaller on one or both sides than the image
