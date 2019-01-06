886a243ff19a1d73aba214c4a210880dccb4b7d3
CRImier <crimier@yandex.ru>
2019-01-07 00:48:50 +0200

    More phone app work - status screen ready

--- apps/phone/main.py
+++ apps/phone/main.py
@@ -1,9 +1,15 @@
+from time import sleep
+
 from apps import ZeroApp
-from ui import Refresher, NumpadNumberInput, Canvas, FunctionOverlay
+from ui import Refresher, NumpadNumberInput, Canvas, MockOutput, FunctionOverlay, \
+   offset_points, get_bounds_for_points, expand_coords, multiply_points, check_coordinates, \
+   convert_flat_list_into_pairs as cflip
 from ui.base_ui import BaseUIElement
 from ui.base_view_ui import BaseView
 
-from time import sleep
+import graphics
+from views import InputScreenView
+
 
 class InputScreen(NumpadNumberInput):
 
@@ -20,88 +26,105 @@ class InputScreen(NumpadNumberInput):
         return d
 
 
-class InputScreenView(BaseView):
-
-    top_offset = 8
-    value_height = 16
-    value_font = ("Fixedsys62.ttf", value_height)
-
-    def __init__(self, o, el):
-        BaseView.__init__(self, o, el)
-        self.c = Canvas(self.o)
-        # storage for text pagination optimisation
-        self.width_cache = {}
-        self.character_width_cache = {}
-        self.build_charwidth_cache()
-        print(self.character_width_cache)
-        self.prev_value_parts = []
-
-    def build_charwidth_cache(self):
-        for char in "".join(self.el.mapping.values()):
-            self.character_width_cache[char] = self.gtb(char, font=self.value_font)[0]
-
-    def gtw(self, text, font):
-        #print("{} - {}".format(text, [self.character_width_cache[c] for c in text]))
-        return sum([self.character_width_cache[c] for c in text])
-
-    def gtb(self, text, font):
-        return self.c.get_text_bounds(text, font=font)
-
-    def get_onscreen_value_parts(self, value_parts):
-        return value_parts[-3:]
-
-    def get_displayed_image(self):
-        self.c.clear()
-        value = self.el.get_displayed_value()
-        value_parts = self.paginate_value(value, self.value_font, width=self.o.width-6, cache=self.prev_value_parts)
-        self.prev_value_parts = value_parts
-        onscreen_value_parts = self.get_onscreen_value_parts(value_parts)
-        for i, value_part in enumerate(onscreen_value_parts):
-            self.c.text(value_part, (3, self.top_offset+self.value_height*i), font=self.value_font)
-        return self.c.get_image()
-
-    def paginate_value(self, value, font, width=None, cache=None):
-        # This function was optimised because I felt like it was kinda slow...
-        # Specifically, because of all the get_text_bounds calls.
-        # Now I'm not sure it was worth it - probably was?
-        width = width if width else self.o.width
-        value_parts = []
-        # Let's optimise the counting a little bit and reuse the results of last
-        # pagination to speed this one up
-        if cache:
-            for part in cache[:-1]: #Except the last part since it's incomplete
-                if value.startswith(part):
-                    value_parts.append(part)
-                    value = value[len(part):]
-        # Now, onto checking the last part - maybe it's overflown,
-        # or maybe it's the first run and we haven't yet calculated the cache
-        # (i.e. someone supplied a long value to __init__ of the UI element)
-        # Let's go from the end - that will be faster for the most likely usecase.
-        while value:
-            counter = 0
-            while counter < len(value):
-                shown_part = value[:-counter] if counter != 0 else value
-                remainder = value[-counter:] if counter != 0 else ""
-                width_of_next = self.width_cache.get(shown_part, None)
-                if width_of_next is None:
-                    width_of_next = self.gtw(shown_part, font)
-                    self.width_cache[shown_part] = width_of_next
-                if width_of_next < width:
-                    break
-                counter += 1
-            value_parts.append(shown_part)
-            value = remainder
-        print(value_parts)
-        return value_parts
-
-
 class StatusScreen(Refresher):
+
+    arrow_x = 15
+    arrow_y = 40
+    handset_x = 2
+    handset_y = 10
+    arrow_offset = 2
+    counter = 0
+    number_frame = (10, 45, "-10", "-1")
+    number_height = 16
+    number_font = ("Fixedsys62.ttf", number_height)
 
     def __init__(self, *args, **kwargs):
        Refresher.__init__(self, self.show_status, *args, **kwargs)
+       self.c = Canvas(self.o)
+       self.prepare_number()
+       self.predraw_arrow()
+       self.predraw_cross()
+       self.predraw_handset()
+       self.status = {"number":"25250034", "accepted":True, "time":385}
+
+    def prepare_number(self):
+       frame_coords = check_coordinates(self.c, self.number_frame)
+       nw, nh = get_bounds_for_points(cflip(frame_coords))
+       self.number_c = Canvas(MockOutput(width=nw, height=nh, device_mode=self.o.device_mode))
+
+    def predraw_arrow(self):
+       aw, ah = get_bounds_for_points(graphics.arrow)
+       arrow_c = Canvas(MockOutput(width=aw, height=ah, device_mode=self.o.device_mode))
+       arrow_c.polygon(graphics.arrow, fill="white")
+       self.arrow_img = arrow_c.get_image()
+
+    def predraw_cross(self):
+       aw, ah = get_bounds_for_points(graphics.cross)
+       cross_c = Canvas(MockOutput(width=aw, height=ah, device_mode=self.o.device_mode))
+       cross_c.polygon(graphics.cross, fill="white")
+       self.cross_img = cross_c.get_image()
+
+    def predraw_handset(self):
+       handset = multiply_points(graphics.phone_handset, 2)
+       aw, ah = get_bounds_for_points(handset)
+       handset_c = Canvas(MockOutput(width=aw, height=ah, device_mode=self.o.device_mode))
+       handset_c.polygon(handset, fill="white")
+       self.handset_img = handset_c.get_image()
+
+    def draw_arrow(self, c, flipped=False):
+       coords = (self.arrow_x, self.arrow_y, self.arrow_x+self.arrow_img.width, self.arrow_y+self.arrow_img.height)
+       clear_coords = expand_coords(coords, self.arrow_offset)
+       c.clear(clear_coords)
+       img = self.arrow_img if not flipped else self.arrow_img.rotate(180)
+       c.image.paste(img, (self.arrow_x, self.arrow_y))
+
+    def draw_cross(self, c, flipped=False):
+       coords = (self.arrow_x, self.arrow_y, self.arrow_x+self.cross_img.width, self.arrow_y+self.cross_img.height)
+       clear_coords = expand_coords(coords, self.arrow_offset)
+       c.clear(clear_coords)
+       c.image.paste(self.cross_img, (self.arrow_x, self.arrow_y))
+
+    def draw_handset(self, c, flipped=False):
+       coords = (self.handset_x, self.handset_y, self.handset_x+self.handset_img.width, self.handset_y+self.handset_img.height)
+       c.image.paste(self.handset_img, (self.handset_x, self.handset_y))
+
+    def get_status(self):
+       states = ["incoming", "outgoing", "missed"]
+       self.status["state"] = states[self.counter]
+       self.counter += 1
+       if self.counter == 3: self.counter = 0
+       return self.status
+
+    def draw_state(self, c, status):
+       if status["state"] == "incoming":
+           self.draw_arrow(c, flipped=True)
+       elif status["state"] == "outgoing":
+           self.draw_arrow(c)
+       elif status["state"] == "missed":
+           self.draw_cross(c)
+
+    def draw_text_state(self, c, status):
+       if not status["accepted"]:
+           self.c.text(status["state"].capitalize(), (30, 9), font=(self.number_font[0], 16))
+       else:
+           status["time"] = status["time"] + 1
+           time_str = "{:02d}:{:02d}".format(*divmod(status["time"], 60))
+           self.c.text(time_str, (30, 9), font=(self.number_font[0], 16))
+
+    def draw_number(self, c, status):
+       self.number_c.clear()
+       self.number_c.centered_text(status["number"], font=self.number_font)
+       c.image.paste(self.number_c.image, self.number_frame[:2])
+       c.rectangle(self.number_frame)
 
     def show_status(self):
-       pass
+       self.c.clear()
+       status = self.get_status()
+       self.draw_number(self.c, status)
+       self.draw_handset(self.c)
+       self.draw_state(self.c, status)
+       self.draw_text_state(self.c, status)
+       return self.c.get_image()
 
 
 class PhoneApp(ZeroApp):
@@ -111,18 +134,27 @@ class PhoneApp(ZeroApp):
     def __init__(self, *args, **kwargs):
         ZeroApp.__init__(self, *args, **kwargs)
         self.input_screen = InputScreen(self.i, self.o)
-        self.insc_overlay = FunctionOverlay(["deactivate", "backspace"], labels=["Exit", "Backspace"])
+        self.insc_overlay = FunctionOverlay({"KEY_F1":"deactivate", "KEY_F2":"backspace", "KEY_ENTER":self.insc_options}, labels=["Cancel", "Menu", "Bckspc"])
         self.insc_overlay.apply_to(self.input_screen)
-        #self.status_screen = StatusScreen(self.i, self.o)
+        self.status_screen = StatusScreen(self.i, self.o, keymap={"KEY_ANSWER":self.accept_call, "KEY_HANGUP":self.reject_call})
 
     def insc_options(self):
-        pass
+        self.switch_to_status_screen()
 
     def get_context(self, c):
         self.context = c
 
     def switch_to_input_screen(self, digit=None):
         self.input_screen.activate()
+
+    def switch_to_status_screen(self, digit=None):
+        self.status_screen.activate()
+
+    def accept_call(self):
+        self.status_screen.status["accepted"] = True
+
+    def reject_call(self):
+        self.status_screen.status["accepted"] = False
 
     def on_call(self):
         self.c.request_switch()
