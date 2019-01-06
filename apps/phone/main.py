85cf72c35eb8f884ad3c2b20b7d973f3d34e56a8
CRImier <crimier@yandex.ru>
2019-01-06 15:56:05 +0200

    More testing and optimizations in the dialer

--- apps/phone/main.py
+++ apps/phone/main.py
@@ -1,19 +1,21 @@
 from apps import ZeroApp
-from ui import Refresher, NumpadCharInput, Canvas, FunctionOverlay
+from ui import Refresher, NumpadNumberInput, Canvas, FunctionOverlay
 from ui.base_ui import BaseUIElement
 from ui.base_view_ui import BaseView
 
-class InputScreen(NumpadCharInput):
+from time import sleep
+
+class InputScreen(NumpadNumberInput):
 
     message = "Input number:"
     default_pixel_view = "InputScreenView"
     def __init__(self, i, o, *args, **kwargs):
         kwargs["message"] = self.message
-        NumpadCharInput.__init__(self, i, o, *args, **kwargs)
-        self.value = "123456789023456789012345678901234567890"
+        self.value = "00000000000000000000000000000000000000000000000000000000012345"
+        NumpadNumberInput.__init__(self, i, o, value=self.value, *args, **kwargs)
 
     def generate_views_dict(self):
-        d = NumpadCharInput.generate_views_dict(self)
+        d = NumpadNumberInput.generate_views_dict(self)
         d.update({"InputScreenView":InputScreenView})
         return d
 
@@ -27,6 +29,20 @@ class InputScreenView(BaseView):
     def __init__(self, o, el):
         BaseView.__init__(self, o, el)
         self.c = Canvas(self.o)
+        # storage for text pagination optimisation
+        self.width_cache = {}
+        self.character_width_cache = {}
+        self.build_charwidth_cache()
+        print(self.character_width_cache)
+        self.prev_value_parts = []
+
+    def build_charwidth_cache(self):
+        for char in "".join(self.el.mapping.values()):
+            self.character_width_cache[char] = self.gtb(char, font=self.value_font)[0]
+
+    def gtw(self, text, font):
+        #print("{} - {}".format(text, [self.character_width_cache[c] for c in text]))
+        return sum([self.character_width_cache[c] for c in text])
 
     def gtb(self, text, font):
         return self.c.get_text_bounds(text, font=font)
@@ -37,21 +53,44 @@ class InputScreenView(BaseView):
     def get_displayed_image(self):
         self.c.clear()
         value = self.el.get_displayed_value()
-        value_parts = self.paginate_value(value, self.value_font, width=self.o.width-6)
+        value_parts = self.paginate_value(value, self.value_font, width=self.o.width-6, cache=self.prev_value_parts)
+        self.prev_value_parts = value_parts
         onscreen_value_parts = self.get_onscreen_value_parts(value_parts)
         for i, value_part in enumerate(onscreen_value_parts):
             self.c.text(value_part, (3, self.top_offset+self.value_height*i), font=self.value_font)
         return self.c.get_image()
 
-    def paginate_value(self, value, font, width=None):
+    def paginate_value(self, value, font, width=None, cache=None):
+        # This function was optimised because I felt like it was kinda slow...
+        # Specifically, because of all the get_text_bounds calls.
+        # Now I'm not sure it was worth it - probably was?
         width = width if width else self.o.width
         value_parts = []
+        # Let's optimise the counting a little bit and reuse the results of last
+        # pagination to speed this one up
+        if cache:
+            for part in cache[:-1]: #Except the last part since it's incomplete
+                if value.startswith(part):
+                    value_parts.append(part)
+                    value = value[len(part):]
+        # Now, onto checking the last part - maybe it's overflown,
+        # or maybe it's the first run and we haven't yet calculated the cache
+        # (i.e. someone supplied a long value to __init__ of the UI element)
+        # Let's go from the end - that will be faster for the most likely usecase.
         while value:
             counter = 0
-            while self.gtb(value[:counter], font)[0] <= width and value[counter:]:
+            while counter < len(value):
+                shown_part = value[:-counter] if counter != 0 else value
+                remainder = value[-counter:] if counter != 0 else ""
+                width_of_next = self.width_cache.get(shown_part, None)
+                if width_of_next is None:
+                    width_of_next = self.gtw(shown_part, font)
+                    self.width_cache[shown_part] = width_of_next
+                if width_of_next < width:
+                    break
                 counter += 1
-            value_parts.append(value[:counter])
-            value = value[counter:]
+            value_parts.append(shown_part)
+            value = remainder
         print(value_parts)
         return value_parts
 
