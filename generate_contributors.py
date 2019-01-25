3abc022a3412ce0180f77b6e66ae41a75acf7383
CRImier <crimier@yandex.ru>
2019-01-25 06:28:28 +0200

    Adding Serge Spraiter to contributors, updating the generate_contributors.py script to sort explicitly by the amount of commits

--- generate_contributors.py
+++ generate_contributors.py
@@ -15,12 +15,19 @@ replace_contributors = {"CRImier": "CRImier (Arsenijs)",
                         "Zero Phone": "Unnamed contributor",
                         "Louis Pi": "Louis Parkerson"}
 
+
+add_contributors = [[10, "Serge Spraiter"]]
+
 output = check_output(["git", "shortlog", "--numbered", "--summary", "--all"])
 lines = filter(None, [line.strip() for line in output.split('\n')])
-contributor_names = [line.split("\t", 1)[-1].strip() for line in lines]
-contributors = [name for name in contributor_names if name not in duplicate_contributors]
+contributors = [line.split("\t", 1) for line in lines]
+contributors = [[int(n), name.strip()] for n, name in contributors]
+for c in add_contributors:
+    contributors.append(c)
+contributors = list(reversed(sorted(contributors)))
+contributor_names = [name for _, name in contributors if name not in duplicate_contributors]
 
 with open("CONTRIBUTORS.md", 'w') as f:
     f.write("#ZPUI project contributors\n\n")
-    for name in contributors:
+    for name in contributor_names:
         f.write(" - {}\n".format(replace_contributors.get(name, name)))
