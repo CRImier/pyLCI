72d0be1a35d729125d359a48f63b28e4bd4d6df3
CRImier <crimier@yandex.ru>
2019-07-18 10:41:18 +0300

    Adding KeyAction, improving inheritance for combining actions, adding tests to make sure that the inheritance works

--- actions.py
+++ actions.py
@@ -1,9 +1,13 @@
+import unittest
+
 class Action(object):
     name = None
     func = None
     type = None
     default_affects_backlight = True
     default_will_context_switch = True
+
+    used_kwargs = []
 
     def __init__(self, name, func, type="simple", \
                  menu_name = None, description = None, \
@@ -22,8 +26,12 @@ class Action(object):
           if affects_backlight is not None \
           else self.default_affects_backlight
         kwargs.pop("context", None)
+        self.set_unused_kwargs_as_properties(kwargs)
+
+    def set_unused_kwargs_as_properties(self, kwargs):
         for key, value in kwargs.items():
-            setattr(self, key, value)
+            if key not in self.used_kwargs:
+                setattr(self, key, value)
 
 class BackgroundAction(Action):
     default_affects_backlight = False
@@ -34,6 +42,33 @@ class FirstBootAction(Action):
 
 class ContextSwitchAction(Action):
     def __init__(self, name, func, **kwargs):
-       Action.__init__(self, name, func, **kwargs)
+       super(ContextSwitchAction, self).__init__(name, func, **kwargs)
        if not hasattr(self, "target_context"):
           self.target_context = None
+
+class KeyAction(Action):
+    used_kwargs = ["key", "key_state"]
+    def __init__(self, *args, **kwargs):
+       key = kwargs.pop("key")
+       state = kwargs.pop("key_state", None)
+       self.key = key
+       self.key_state = state
+       super(KeyAction, self).__init__(*args, **kwargs)
+
+# Tests
+
+class TestActions(unittest.TestCase):
+    def test_combined_contextswitch_key_action(self):
+        """Testing whether inheritance properly combines __init__ of both classes."""
+        pass
+        cls = type("TestAction", (ContextSwitchAction, KeyAction), {})
+        action = cls("testaction", lambda x:True, target_context="main", key="KEY_ENTER", key_state=0)
+        assert(getattr(action, "target_context", None) == "main")
+        assert(getattr(action, "key", None) == "KEY_ENTER")
+        assert(getattr(action, "key_state", None) == 0)
+        action = cls("testaction", lambda x:True, target_context="main", key="KEY_ENTER")
+        assert(getattr(action, "target_context", None) == "main")
+        assert(getattr(action, "key", None) == "KEY_ENTER")
+
+if __name__ == "__main__":
+    unittest.main()
