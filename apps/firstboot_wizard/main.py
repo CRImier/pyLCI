4874c7eb080dce2186d3f04f76f6e502b9449fc8
CRImier <crimier@yandex.ru>
2019-11-10 18:18:44 +0200

    apps/firstboot_wizard: refactored the current code a bit

--- apps/firstboot_wizard/main.py
+++ apps/firstboot_wizard/main.py
@@ -22,40 +22,47 @@ class FirstbootWizard(ZeroApp):
     def execute_after_contexts(self):
         self.do_firstboot()
 
-    def do_firstboot(self):
-        return # WIP code - works, but is unfinished
+    def get_firstboot_file(self):
+        """ Returns ``(filename, is_new_file)``, ``(None, False)`` on failure """
         file = None
-        new_file = False
         for file in firstboot_file_locations:
             if os.path.exists(file):
                 logger.info("Firstboot list file {} found")
-                break
+                return (file, False)
         else:
             logger.info("No firstboot list files found, creating one")
             for file_path in firstboot_file_locations:
                 # try and touch the file
                 try:
                     open(file_path, "w").close()
-                    new_file = True
                 except:
                     logger.exception("Can't create a firstboot list file at {}, skipping".format(file_path))
                 else:
                     logger.info("Created a firstboot list file at {}".format(file_path))
-                    break
+                    return (file, True)
             else:
                 # We can't create any files!
-                logger.error("Can't create a firstboot list file! Locations: {}".format(firstboot_file_locations))
-                return
+                logger.error("Can't create any of the firstboot list files! Locations: {}".format(firstboot_file_locations))
+                return (None, False)
+
+    def get_completed_actions_from_file(self, file):
+        try:
+            with open(file, 'r') as f:
+                return [l.strip() for l in f.readlines() if l.strip()]
+        except:
+            logger.exception("Couldn't load completed actions from firstboot list file")
+            return []
+
+    def do_firstboot(self):
         firstboot_actions = cm.am.get_firstboot_actions()
         firstboot_action_names = list(firstboot_actions.keys())
-        executed_action_names = []
+        file, is_new_file = self.get_firstboot_file()
+        if file is None:
+            logger.error("Can't read/create a firstboot file, no sense for the firstboot application to continue")
+            return
         completed_action_names = []
         if not new_file:
-          try:
-            with open(file, 'r') as f:
-                completed_action_names = [l.strip() for l in f.readlines() if l.strip()]
-          except:
-            logger.exception("Couldn't load completed actions from firstboot list file")
+            completed_action_names = self.get_completed_actions_from_file(file)
         non_completed_action_names = [n for n in firstboot_action_names if n not in completed_action_names]
         if non_completed_action_names:
             if not completed_action_names: # Not the first boot - some actions have been completed before
@@ -68,7 +75,6 @@ class FirstbootWizard(ZeroApp):
             choice = DialogBox('yn', self.i, self.o, message=message, name="Firstboot wizard setup menu").activate()
             if not choice:
                 self.context.rescind_exclusive()
-                #self.context.signal_finished()
                 return
             else:
                 # User confirmed that they want to go through with the firstboot wizard
