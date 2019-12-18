10c3762dc4d690965a3e7fca0d698ce69552fa55
CRImier <crimier@yandex.ru>
2019-12-18 09:14:50 +0200

    apps/system_apps/shutdown: adding a "reboot after making changes?" firstboot action prompt

--- apps/system_apps/shutdown/main.py
+++ apps/system_apps/shutdown/main.py
@@ -1,8 +1,14 @@
 menu_name = "Shutdown&reboot"
 
 from subprocess import call
-from ui import Menu
+from ui import Menu, DialogBox
+from actions import FirstBootAction
 
+context = None
+def set_context(c):
+    global context
+    context = c
+    context.register_firstboot_action(FirstBootAction("reboot_after_firstboot", reboot_after_firstboot, depends=["change_wifi_country"], not_on_emulator=True))
 
 def shutdown():
     o.clear()
@@ -13,6 +19,17 @@ def reboot():
     o.clear()
     o.display_data("Rebooting")
     call(['reboot'])
+
+def reboot_after_firstboot():
+    choice = DialogBox("ync", i, o, message="Reboot to apply changes?", name="Reboot app firstboot reboot dialog").activate()
+    if not choice:
+        return True
+    o.clear()
+    o.display_data("Rebooting")
+    call(['reboot'])
+    return True
+
+
 
 #Some globals for LCS
 main_menu = None
