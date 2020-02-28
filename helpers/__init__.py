6d28c5b681903bec9eb451bb54d4a3e726be5e0b
CRImier <crimier@yandex.ru>
2020-02-28 17:21:13 +0200

    helpers/input_system: adding a get_all_available_keys method, documenting the cb_needs_key_state method

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,5 +4,5 @@ from runners import BooleanEvent, Oneshot, BackgroundRunner
 from usability import ExitHelper, remove_left_failsafe
 from logger import setup_logger
 from process import ProHelper
-from input_system import KEY_RELEASED, KEY_HELD, KEY_PRESSED, cb_needs_key_state
+from input_system import KEY_RELEASED, KEY_HELD, KEY_PRESSED, cb_needs_key_state, get_all_available_keys
 from env import zpui_running_as_service, is_emulator
--- helpers/input_system.py
+++ helpers/input_system.py
@@ -5,6 +5,15 @@ KEY_RELEASED = 0
 KEY_HELD = 2
 
 def cb_needs_key_state(callback, new_function=False):
+    """
+    Marks a callable with an attribute that lets the input system know that, on key event,
+    key state should be passed to this callable. As a result, the callable will also be called
+    on key press, hold and release, instead of only being called on a keypress, as is the default.
+
+    By default, just sets an attribute on the callable object passed - if that fails
+    (i.e. you have a lambda or a class method), pass ``new_function=True`` keyword argument
+    to wrap the callable.
+    """
     if new_function:
         @wraps(callback)
         def func(*args, **kwargs):
@@ -17,3 +26,12 @@ def cb_needs_key_state(callback, new_function=False):
     else:
         callback.zpui_icb_pass_key_state = True
     return callback
+
+def get_all_available_keys(i):
+    """
+    Given an input proxy, returns a list of all keys that are available
+    from all the input drivers currently attached.
+    """
+    l = []
+    [l.extend(v) for v in i.available_keys.values()]
+    return list(set(l))
